50e0cc7017a6cdaffcec4b9c2db5dd8a1c2d33a1
Roman Schiefer <roman.schiefer@gmail.com>
2017-04-11 11:00:11 +0200

    added floating action button, needs to be moved to correct position

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 import OverviewTable from './OverviewTable';
 
@@ -10,13 +12,20 @@ const Overview = ({ streams }) => (
     top: '100px',
     position: 'absolute',
     zIndex: 1100,
-
   }}>
     <CardTitle title="Streams" subtitle="Overview of existing workflows" />
     <CardText>
       All workflows are listed below.
     </CardText>
     <OverviewTable streams={streams}/>
+    <FloatingActionButton secondary style={{
+        position: 'absolute',
+        right: '-28px',
+        top: '16px'
+      }}
+      tooltip="test">
+      <ContentAdd />
+    </FloatingActionButton>
   </Card>
 );
 
