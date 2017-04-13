41da1ca01d59c82330ddc1f4c4116fc7cb7b8b2d
Roman Schiefer <roman.schiefer@gmail.com>
2017-04-13 16:50:28 +0200

    added loading indication

---
+++ src/pages/Loading/Loading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+
+const Loading = (props) => (
+  <div style={{
+    display: 'flex',
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    zIndex: 2000
+  }}>
+    <CircularProgress size={80} thickness={5} />
+  </div>
+);
+
+
+export default Loading;
