3ba812c18e2d7b01a9a6a54254ade732e1c83362
Roman Schiefer <roman.schiefer@gmail.com>
2017-04-15 12:36:14 +0200

    added default map implementation

--- src/pages/Dashboard/NetworkMap.js
+++ src/pages/Dashboard/NetworkMap.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Map, MarkerGroup } from 'react-d3-map';
 
-const NetworkMap = (props) => (
-  <h1>Network Map</h1>
-)
+const NetworkMap = () => {
+  var data = {
+    "type": "Feature",
+    "properties": {
+      "text": "this is a Point!!!"
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [122, 23.5]
+    }
+  }
+  var width = 700;
+  var height = 700;
+  // set your zoom scale
+  var scale = 1200 * 5;
+  // min and max of your zoom scale
+  var scaleExtent = [1 << 12, 1 << 13]
+  // set your center point
+  var center = [122, 23.5];
+  // set your popupContent
+  var popupContent = function (d) { return d.properties.text; }
+
+
+  return (
+    <Map
+      width={width}
+      height={height}
+      scale={scale}
+      scaleExtent={scaleExtent}
+      center={center}
+    >
+      <MarkerGroup
+        key={"polygon-test"}
+        data={data}
+        popupContent={popupContent}
+        markerClass={"your-marker-css-class"}
+      />
+    </Map>
+  );
+}
 
 export default NetworkMap;
