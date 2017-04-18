6cdd9325ed852a081ba4931f463083d3f984975e
jzakotnik <jure@zakotnik.de>
2017-04-18 22:15:41 +0200

    Changed colors to official hex codes of the style guide

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,16 @@
 const colors = {
-  lightColor: 'white'
+  lightColor: 'white'  
 }
+
+var ACMECorpLightgrey = exports.ACMECorpLightgrey = '#f0ebe6'
+var ACMECorpGrey = exports.ACMECorpGrey = '#beb9b4'
+var ACMECorpDarkgrey = exports.ACMECorpDarkgrey = '#5a6166'
+var ACMECorpLightgreen = exports.ACMECorpLightgreen = '#60800f'
+var ACMECorpGreen = exports.ACMECorpGreen = '#4e6e61'
+var ACMECorpBlueGreen = exports.ACMECorpBlueGreen = '#255166'
+var ACMECorpLightblue = exports.ACMECorpLightblue = '#cde1eb'
+var ACMECorpBlue = exports.ACMECorpBlue = '#5a9bbe'
+var ACMECorpDarkBlue = exports.ACMECorpDarkBlue = '#1b618c'
+
 
 export default colors;
--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import createHistory from 'history/createBrowserHistory'
 
-import {blue900} from 'material-ui/styles/colors';
-import {blueGrey800} from 'material-ui/styles/colors';
+//import {blue900} from 'material-ui/styles/colors';
+//import {blueGrey800} from 'material-ui/styles/colors';
+import {ACMECorpLightgrey} from './colors'
+import {ACMECorpGrey} from './colors'
+import {ACMECorpDarkgrey} from './colors'
+import {ACMECorpLightgreen} from './colors'
+import {ACMECorpGreen} from './colors'
+import {ACMECorpBlueGreen} from './colors'
+import {ACMECorpLightblue} from './colors'
+import {ACMECorpBlue} from './colors'
+import {ACMECorpDarkBlue} from './colors'
 
 
 import Main from './pages/Main/Main';
@@ -23,8 +32,13 @@ injectTapEventPlugin();
 
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: blue900,
-    accent1Color: blueGrey800,
+    primary1Color: ACMECorpLightgreen,
+    primary2Color: ACMECorpBlue,
+    primary3Color: ACMECorpLightgrey,
+    accent1Color: ACMECorpGreen,
+    accent2Color: ACMECorpLightblue,
+    accent3Color: ACMECorpGrey,
+    alternateTextColor: ACMECorpLightgrey,
   },
 });
 
