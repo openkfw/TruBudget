969dc5a613d278976fa5e22d0994de37e3f92945
DKVZGDWW <sascha.kerbler@gmail.com>
2017-05-04 09:09:44 +0200

    flexboxes for network map and not found page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,19 +4,18 @@ import { Card, CardTitle, CardText } from 'material-ui/Card';
 import NetworkMap from './NetworkMap';
 
 const Dashboard = (props) => (
-  <Card style={{
-    width: '80%',
-    left: '10%',
-    top: '100px',
-    position: 'absolute',
-    zIndex: 1100,
-  }}>
-    <CardTitle title="The Blockchain network dashboard" subtitle="Connected peers in the blockchain network" />
-    <CardText>
-      The connected blockchain nodes are shown in the map below. You can click on the respective markers to obtain the exact location.
+  <div style={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'center', marginTop: '50px', }}>
+    <Card style={{
+      width: '80%',
+      position: 'relative',
+    }}>
+      <CardTitle title="The Blockchain network dashboard" subtitle="Connected peers in the blockchain network" />
+      <CardText>
+        The connected blockchain nodes are shown in the map below. You can click on the respective markers to obtain the exact location.
     </CardText>
-    <NetworkMap nodeInformation={props.nodeInformation} />
-  </Card>
+      <NetworkMap nodeInformation={props.nodeInformation} />
+    </Card>
+  </div>
 )
 
 export default Dashboard;
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Card, CardText } from 'material-ui/Card';
 
 const NotFound = () => (
-  <Card style={{
-    width: '60%',
-    left: '20%',
-    top: '100px',
-    position: 'absolute',
-    zIndex: 1100
-  }}>
-    <CardText style={{
-      textAlign: 'center'
+  <div style={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'center', marginTop: '50px', }}>
+    <Card style={{
+      width: '60%',
+      position: 'relative',
+      zIndex: 1100
     }}>
-      <h4>404 - Sorry, I couldn't find the page you requested</h4>
-      <br />
-      <img src="/404.gif" alt="I am sorry :(" />
-    </CardText>
-  </Card>
+      <CardText style={{
+        textAlign: 'center'
+      }}>
+        <h4>404 - Sorry, I couldn't find the page you requested</h4>
+        <br />
+        <img src="/404.gif" alt="I am sorry :(" />
+      </CardText>
+    </Card>
+  </div>
 )
 
 export default NotFound;
