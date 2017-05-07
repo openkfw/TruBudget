24a474d4ffe55951cf7ba78f7f9c5804703832d7
jzakotnik <jure@zakotnik.de>
2017-05-07 21:31:37 +0200

    removed lorem ipsum

--- src/pages/Notifications/NotificationPage.js
+++ src/pages/Notifications/NotificationPage.js
@@ -23,10 +23,7 @@ const NotificationPage = ({ list, streamNames, users, loggedInUser, markNotifica
       <Card style={{ width: '60%', marginBottom: '10px' }}>
         <CardTitle title="Notifications" subtitle="Unread" />
         <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          Please find your current notifications below. These display action items or information items to be dealt with.
     </CardText>
         <NotificationTable
           notifications={list}
