d6b7bb5191cf3edab5c6ae76fff3c24bb4b3ce4f
Roman Schiefer <roman.schiefer@gmail.com>
2017-11-28 16:24:20 +0100

    blockchain is now able to connect itself to other networks

--- src/connectToChain.js
+++ src/connectToChain.js
@@ -2,19 +2,23 @@ const axios = require('axios');
 const { spawn } = require('child_process');
 const fs = require('fs');
 
-const masterIP = process.env.MASTER_API_IP || '127.0.0.1';
+const masterIP = process.env.MASTER_API || '127.0.0.1';
 const masterPort = process.env.MASTER_API_PORT || '8080';
 const chainName = process.env.CHAINNAME || 'ACMECorpChain';
 const masterChainIP = process.env.MASTERNODE_IP || '127.0.0.2';
 const masterChainPort = process.env.NETWORK_PORT || 7447;
 const organization = process.env.ORGANIZATION || 'testorg';
 
+let address = undefined;
+
 const relax = async (ms) => new Promise(res => setInterval(res, ms))
 
 const startMultichainDaemon = () => {
     const mc = spawn('multichaind', [`${chainName}@${masterChainIP}:${masterChainPort}`, " -shrinkdebugfilesize"]);
-    mc.on('data', (data) => {
-        console.log(`-> ${data}`);
+    mc.stdout.on('data', (data) => {
+        const regex = new RegExp('[0-9a-zA-Z]{30,40}');
+        const match = regex.exec(data);
+        if (match) address = match[0];
     })
 
     mc.on('close', async (code) => {
@@ -28,13 +32,10 @@ const startMultichainDaemon = () => {
     });
 }
 
-const getAddressFromLog = () => new Promise((res, rej) => {
-    fs.readFile(`/root/.multichain/${chainName}/debug.log`, 'utf8', (err, data) => {
-        if (err) return rej(err);
-        const regex = new RegExp('[0-9a-zA-Z]{30,40}');
-        return res(regex.exec(data)[0])
-    })
-})
+const waitForAddress = async () => {
+    await relax(5000);
+    if (!address) await waitForAddress();
+}
 
 const askMasterForPermissions = (address, organization) => axios.post(`http://${masterIP}:${masterPort}/nodes/`, {
     address,
@@ -43,7 +44,7 @@ const askMasterForPermissions = (address, organization) => axios.post(`http://${
 
 const registerNodeAtMaster = async () => {
     try {
-        const address = await getAddressFromLog();
+        await waitForAddress();
         console.log(`>>> Access: Requesting Master to grant access for me (${address})`);
         await askMasterForPermissions(address, organization);
         console.log('>>> Access: Success!');
