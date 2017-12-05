1dfd8a63f0382e2ed9df210c06d67dae78b9c09c
sascha <sascha.kerbler@gmail.com>
2017-12-05 11:28:56 +0100

    listening on Enter for role selection

--- src/pages/Overview/RoleSelectionContent.js
+++ src/pages/Overview/RoleSelectionContent.js
@@ -52,7 +52,7 @@ class RoleSelectionContent extends Component {
   captureEnterClick = (event) => {
     if (event.charCode === 13) {
       const unSelectedDataSource = _.differenceBy(this.props.dataSource, this.props.selections);
-      const index = unSelectedDataSource.findIndex((role) => role.toLowerCase() === _.trim(this.state.searchText).toLowerCase())
+      const index = unSelectedDataSource.findIndex((role) => role.organization.toLowerCase() === _.trim(this.state.searchText).toLowerCase())
       if (index > -1) {
         const role = unSelectedDataSource[index];
         this.onSelect(role, index);
