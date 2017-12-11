beb57df88b213bedffdfe86a05dfe8c3d4f906cf
sascha <sascha.kerbler@gmail.com>
2017-12-11 13:12:44 +0100

    dockerfiles adapted

--- configureServer.sh
+++ configureServer.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-/usr/bin/sed "/proxy_pass/d" ./nginx2.conf > ./test.conf
+/usr/bin/sed -i "/proxy_pass/d" ./nginx.conf
+# Check if the required env variables are set otherwise localhost will be used.
+if [ "$PROD_API_HOST" ] && [ "$PROD_API_PORT" ]; then
+  prod_host=$PROD_API_HOST
+  prod_port=$PROD_API_PORT
+else
+  prod_host=localhost
+  prod_port=8080
+fi
 
-/usr/bin/sed -e "/# pathToApi/i\\
-proxy_pass http://trubudget-api-master:8080/;" ./test.conf > ./test2.conf
+if [ "$TEST_API_HOST" ] && [ "$TEST_API_PORT" ]; then
+  test_host=$TEST_API_HOST
+  test_port=$TEST_API_PORT
+else
+  test_host=localhost
+  test_port=8080
+fi
 
+# add the proxy pass and store the conf into the nginx conf directory
+/usr/bin/sed -i -e "/# pathToApi/i\\
+  proxy_pass http://$prod_host:$prod_port/;" ./nginx.conf
 /usr/bin/sed -e "/# pathToTestApi/i\\
-proxy_pass http://trubudget-api-slave:8080/;" ./test2.conf > ./nginx2.conf
-
+  proxy_pass http://$test_host:$test_port/;" ./nginx.conf > /etc/nginx/conf.d/default.conf
 
 rm -rf test.conf test2.conf
-start nginx
+nginx
+
