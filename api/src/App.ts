cf57b8ba0b2abae95fbc1bf33e9ee699d482545c
Kevin Bader <kevin.bader@accenture.com>
2018-04-09 11:13:20 +0200

    api: Add health-check endpoint

--- api/src/App.ts
+++ api/src/App.ts
@@ -25,6 +25,8 @@ const fun = (intent: SimpleIntent) => {
   }
 };
 
+router.get("/health", (req, res) => res.status(200).send("OK"));
+
 router.get("/project.list", async (req, res) => {
   // Returns all projects the user is allowed to see
   const user = req.params.user || "alice";
@@ -47,36 +49,6 @@ router.post("/project.create", async (req, res) => {
     res.status(500).send("INTERNAL SERVER ERROR");
   }
 });
-
-// router.put("/:user/projects/:id", (req, res) => {
-//   const user = req.params.user;
-//   const title = req.body.title;
-//   const projects = runIfAuthorized(user, Project.changeTitle(id, title));
-//   res.json(projects);
-// });
-
-// router.post("/:user/projects/:id/subprojects/:title", (req, res) => {
-//   // Create a subproject only if the user is allowed to:
-//   const user = req.params.user;
-//   const projectId = req.params.id;
-//   const title = req.params.title; // TODO ;-)
-//   const projects = authorize(user, Subproject.create(projectId, title));
-//   res.json(projects);
-// });
-
-// router.get("/projects/:id/subprojects", (req, res) => {
-//   const projectId = req.params.id;
-//   // Lists all subprojects of a project the user is allowed to see:
-//   const subProjects = Project.pick(projectId).listSubprojectsAs(user);
-//   res.json(subProjects);
-// });
-
-// router.post("/projects/:projectId/subprojects/:id/workflows", (req, res) => {
-//   const projectId = req.params.id;
-//   // Lists all workflows of a subproject of a project the user is allowed to see:
-//   const workflows = Project.pick(projectId).pickSubproject(id).listWorkflowsAs(user);
-//   res.json(workflows);
-// });
 
 app.use("/", router);
 
