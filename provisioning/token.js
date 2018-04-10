85bdc99f55060c94b8b43a3fccf6f454fe7a6647
Kevin Bader <kevin.bader@accenture.com>
2018-04-10 10:39:33 +0200

    Revert "Add skeleton for provisioning scripts"
    This reverts commit 57b7a8cc033185dd55e054380ee19e88dda938ec.

--- provisioning/token.js
+++
@@ -1,6 +0,0 @@
-const jsonwebtoken = require("jsonwebtoken");
-
-const getUserToken = (userId, secret) =>
-  jsonwebtoken.sign({ user: userId }, secret, { expiresIn: "1h" });
-
-export const getRootToken = secret => getUserToken("root", secret);
