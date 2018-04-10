289b0154d224b0d8fee773669c314452c4ed9274
Sascha <sascha.kerbler@accenture.com>
2018-04-10 15:50:24 +0200

    API: Add automatic user provisioning on startup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,12 @@
 import app from './App'
 
+import { injectUsers } from "./provisioning/users";
 const port = process.env.PORT || 3000
 
 app.listen(port, (err) => {
   if (err) {
     return console.log(err)
   }
-
+  injectUsers()
   return console.log(`server is listening on ${port}`)
 })
