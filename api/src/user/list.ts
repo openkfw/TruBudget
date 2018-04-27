e4ccc2bd06502d232dbd648c9627491417175209
Kevin Bader <kevin.bader@accenture.com>
2018-04-27 10:22:00 +0200

    api: Refactor user.list

--- api/src/user/list.ts
+++ api/src/user/list.ts
@@ -17,30 +17,21 @@ export const getUserList = async (
   multichain: MultichainClient,
   req: AuthenticatedRequest
 ): Promise<HttpResponse> => {
-  return [
-    200,
-    {
-      apiVersion: "1.0",
-      data: { items: await list(multichain, req.token) }
-    }
-  ];
-};
-
-const list = async (
-  multichain: MultichainClient,
-  token: AuthToken
-): Promise<User.UserWithoutPassword[]> => {
   const users = await User.getAll(multichain);
 
-  if (users.length === 0) {
-    return [];
-  }
+  // users are not filtered for now (user.list and user.view is always allowed)
 
-  // TODO users are not filtered for now (user.list and user.view is always allowed)
-
-  return users.map(user => ({
+  const passwordlessUsers = users.map(user => ({
     id: user.id,
     displayName: user.displayName,
     organization: user.organization
   }));
+
+  return [
+    200,
+    {
+      apiVersion: "1.0",
+      data: { items: passwordlessUsers }
+    }
+  ];
 };
