7e9a87c6dd0733f9eece512798a3aed23579415e
Sascha <sascha.kerbler@accenture.com>
2018-05-04 11:26:58 +0200

    ui: Update workflowItem creation with amountType N/A
    From now on amount and currency are null for workflowItems with amountType N/A

--- frontend/src/pages/Workflows/WorkflowCreation.js
+++ frontend/src/pages/Workflows/WorkflowCreation.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import CreationDialog from '../Common/CreationDialog';
-import strings from '../../localizeStrings'
-import TextInput from '../Common/TextInput';
-import WorkflowCreationAmount from './WorkflowCreationAmount';
-import DocumentUpload from '../Documents/DocumentUpload';
+import React from "react";
+import CreationDialog from "../Common/CreationDialog";
+import strings from "../../localizeStrings";
+import TextInput from "../Common/TextInput";
+import WorkflowCreationAmount from "./WorkflowCreationAmount";
+import DocumentUpload from "../Documents/DocumentUpload";
 
-const handleSubmit = (props) => {
-  const { createWorkflowItem, editWorkflowItem, onWorkflowDialogCancel, editMode, workflowToAdd, workflowDocuments } = props;
+const handleSubmit = props => {
+  const {
+    createWorkflowItem,
+    editWorkflowItem,
+    onWorkflowDialogCancel,
+    editMode,
+    workflowToAdd,
+    workflowDocuments
+  } = props;
   const subproject = props.match.params.subproject;
   if (editMode) {
-    const currentWorkflowItem = props.workflowItems.find((item) => item.txid === props.workflowToAdd.txId);
+    const currentWorkflowItem = props.workflowItems.find(item => item.id === props.workflowToAdd.txId);
     const { key, data } = currentWorkflowItem;
-    editWorkflowItem(subproject, key, workflowToAdd, workflowDocuments, data)
+    editWorkflowItem(subproject, key, workflowToAdd, workflowDocuments, data);
   } else {
-    createWorkflowItem(workflowToAdd, workflowDocuments)
+    createWorkflowItem(workflowToAdd, workflowDocuments);
   }
   onWorkflowDialogCancel();
-}
+};
 
-const WorkflowCreation = (props) => {
+const WorkflowCreation = props => {
   const steps = [
     {
       title: strings.workflow.workflow_name,
       content: (
-        <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-          <TextInput onChange={props.storeWorkflowName}
+        <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+          <TextInput
+            onChange={props.storeWorkflowName}
             value={props.workflowToAdd.name}
             floatingLabelText={strings.workflow.workflow_title}
             hintText={strings.workflow.workflow_title_description}
@@ -34,21 +42,24 @@ const WorkflowCreation = (props) => {
     },
     {
       title: strings.common.budget,
-      content: <WorkflowCreationAmount
-        subProjectCurrency={props.currency}
-        storeWorkflowAmount={props.storeWorkflowAmount}
-        storeWorkflowAmountType={props.storeWorkflowAmountType}
-        storeWorkflowCurrency={props.storeWorkflowCurrency}
-        workflowAmount={props.workflowToAdd.amount}
-        workflowAmountType={props.workflowToAdd.amountType}
-        workflowCurrency={props.workflowToAdd.currency}
-      />
+      content: (
+        <WorkflowCreationAmount
+          subProjectCurrency={props.currency}
+          storeWorkflowAmount={props.storeWorkflowAmount}
+          storeWorkflowAmountType={props.storeWorkflowAmountType}
+          storeWorkflowCurrency={props.storeWorkflowCurrency}
+          workflowAmount={props.workflowToAdd.amount}
+          workflowAmountType={props.workflowToAdd.amountType}
+          workflowCurrency={props.workflowToAdd.currency}
+        />
+      )
     },
     {
       title: strings.common.comment,
       content: (
-        <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-          <TextInput onChange={props.storeWorkflowComment}
+        <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+          <TextInput
+            onChange={props.storeWorkflowComment}
             value={props.workflowToAdd.comment}
             multiLine={true}
             floatingLabelText={strings.workflow.workflow_comment}
@@ -60,9 +71,8 @@ const WorkflowCreation = (props) => {
     {
       title: strings.workflow.workflow_documents,
       content: <DocumentUpload addDocument={props.addDocument} workflowDocuments={props.workflowDocuments} />
-    },
-  ]
-
+    }
+  ];
 
   return (
     <CreationDialog
@@ -73,8 +83,9 @@ const WorkflowCreation = (props) => {
       handleSubmit={handleSubmit}
       steps={steps}
       numberOfSteps={steps.length}
-      {...props} />
-  )
-}
+      {...props}
+    />
+  );
+};
 
 export default WorkflowCreation;
