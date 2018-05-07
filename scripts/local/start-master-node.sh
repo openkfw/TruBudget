d3403623067b52eb3fe0f5249ddd7916899f0f8c
Sascha <sascha.kerbler@accenture.com>
2018-05-07 09:42:29 +0200

    deployment: Stop containers before trying to start new ones

--- scripts/local/start-master-node.sh
+++ scripts/local/start-master-node.sh
@@ -1,3 +1,5 @@
+docker-compose -f docker/local/master-node.yml down 
 git pull 
+
 docker rm $(docker ps -q -f status=exited)
 docker-compose -p local -f docker/local/master-node.yml up --build
--- scripts/local/start-slave-node.sh
+++
@@ -1,3 +0,0 @@
-git pull 
-docker rm $(docker ps -q -f status=exited)
-docker-compose -p local -f docker/local/slave-node.yml up --build
