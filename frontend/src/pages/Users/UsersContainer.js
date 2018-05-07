3b18a27bae37f619213281304e37d952637a5266
Sascha <sascha.kerbler@accenture.com>
2018-05-07 17:28:04 +0200

    ui: Show user-management portal only to users with the corresponding intents

--- frontend/src/pages/Users/UsersContainer.js
+++ frontend/src/pages/Users/UsersContainer.js
@@ -3,17 +3,23 @@ import { connect } from "react-redux";
 import withInitialLoading from "../Loading/withInitialLoading";
 import { toJS } from "../../helper";
 import Users from "./Users";
+import NotFound from "../NotFound/NotFound";
 
 class UsersContainer extends Component {
   render() {
-    return <Users {...this.props} />;
+    //TODO: Change the intents to a more fine grain list
+    const canViewUsers = this.props.allowedIntents.indexOf("global.createUser") > -1;
+    if (canViewUsers) {
+      return <Users {...this.props} />;
+    } else {
+      return <NotFound />;
+    }
   }
 }
 
 const mapStateToProps = state => {
   return {
-    show: state.getIn(["detailview", "showProjectAssignees"]),
-    users: state.getIn(["login", "user"])
+    allowedIntents: state.getIn(["login", "allowedIntents"])
   };
 };
 
