bb55cc8af345a37585d4428ec033855166f69dd0
Sascha <sascha.kerbler@accenture.com>
2018-05-07 11:57:52 +0200

    deployment: Add image tag name to compose env variables

--- scripts/master/start-master-node.sh
+++ scripts/master/start-master-node.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker-compose -f docker-compose/master/master-node.yml down 
-docker rm $(docker ps -f status=exited|grep master_|awk '{print $1}')
+docker rm $(docker ps -q --filter status=exited --filter label=com.docker.compose.project=trubudget)
 
 echo "Login to Docker-Registry(index.docker.io)"
 cat DOCKER_REGISTRY_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin index.docker.io
 
 docker-compose -f docker-compose/master/master-node.yml pull 
-docker-compose -p master -f docker-compose/master/master-node.yml up --build
+docker-compose -p trubudget -f docker-compose/master/master-node.yml up --build
