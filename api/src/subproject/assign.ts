fa6008d924d182687d703f5cf99a8cd8534b38f6
Kevin Bader <kevin.bader@accenture.com>
2018-05-24 17:59:23 +0200

    api: Refactor subproject.assign controller

--- api/src/subproject/assign.ts
+++ api/src/subproject/assign.ts
@@ -1,8 +1,10 @@
 import { throwIfUnauthorized } from "../authz";
 import Intent from "../authz/intents";
+import { AuthToken } from "../authz/token";
 import { AuthenticatedRequest, HttpResponse } from "../httpd/lib";
 import { isNonemptyString, value } from "../lib/validation";
 import { MultichainClient } from "../multichain";
+import { Event } from "../multichain/event";
 import { createNotification } from "../notification/create";
 import * as Subproject from "./model/Subproject";
 
@@ -25,35 +27,64 @@ export const assignSubproject = async (
     await Subproject.getPermissions(multichain, projectId, subprojectId),
   );
 
+  const publishedEvent = await sendEventToDatabase(
+    multichain,
+    req.token,
+    userIntent,
+    userId,
+    projectId,
+    subprojectId,
+  );
+
+  await notifySubprojectAssignee(multichain, req.token, projectId, subprojectId, publishedEvent);
+
+  return [200, { apiVersion: "1.0", data: "OK" }];
+};
+
+async function sendEventToDatabase(
+  multichain: MultichainClient,
+  token: AuthToken,
+  userIntent: Intent,
+  userId: string,
+  projectId: string,
+  subprojectId: string,
+): Promise<Event> {
   const event = {
     intent: userIntent,
-    createdBy: req.token.userId,
+    createdBy: token.userId,
     creationTimestamp: new Date(),
     dataVersion: 1,
     data: { userId },
   };
-
   const publishedEvent = await Subproject.publish(multichain, projectId, subprojectId, event);
+  return publishedEvent;
+}
 
-  // If the subproject has been assigned to someone else, that person is notified about the change:
-  const subproject = await Subproject.get(multichain, req.token, projectId, subprojectId).then(
-    x => x[0],
+/**
+ * If the subproject is assigned to someone else, that person is notified about the
+ * change.
+ */
+async function notifySubprojectAssignee(
+  multichain: MultichainClient,
+  token: AuthToken,
+  projectId: string,
+  subprojectId: string,
+  publishedEvent: Event,
+): Promise<void> {
+  const subproject = await Subproject.get(multichain, token, projectId, subprojectId).then(
+    x => (x.length ? x[0] : undefined),
   );
-  if (subproject.data.assignee !== undefined && subproject.data.assignee !== req.token.userId) {
-    await createNotification(
-      multichain,
-      [{ id: subprojectId, type: "subproject" }, { id: projectId, type: "project" }],
-      req.token.userId,
-      subproject.data.assignee,
-      publishedEvent,
-    );
-  }
 
-  return [
-    200,
-    {
-      apiVersion: "1.0",
-      data: "OK",
-    },
-  ];
-};
+  if (subproject === undefined) return;
+  const assignee = subproject.data.assignee;
+
+  if (assignee === undefined || assignee === token.userId) return;
+
+  await createNotification(
+    multichain,
+    [{ id: subprojectId, type: "subproject" }, { id: projectId, type: "project" }],
+    token.userId,
+    assignee,
+    publishedEvent,
+  );
+}
