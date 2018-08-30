ad18e813edff5ff0a033925a8b6cfadaad62c480
stefan.ziffer <stefan.ziffer@accenture.com>
2018-08-30 17:37:18 +0200

    api: update api-doc for validate Document

--- api/src/httpd/router.ts
+++ api/src/httpd/router.ts
@@ -1202,7 +1202,35 @@ export const createRouter = (
   });
 
   /**
-   * @api {post} /workflowitem.intent.revokePermission Revoke permission
+   * @api {post} /workflowitem.validateDocument Validate Document
+   * @apiVersion 1.0.0
+   * @apiName workflowitem.validateDocument
+   * @apiGroup Workflowitem
+   * @apiPermission user
+   * @apiDescription Validates if the hashed base64 string equals the hash sent by the user.
+   *
+   * @apiParam {String} apiVersion Version of the request layout (e.g., "1.0").
+   * @apiParam {Object} data Request payload.
+   * @apiParam {String} data.base64String base64-string which will be hashed and compared to the other hash value.
+   * @apiParam {String} data.hash hash value which shall be compared with the hashed base64-string.
+   * @apiParamExample {json} Request
+   *   {
+   *   "apiVersion": "1.0",
+   *   "data": {
+   *     "base64String": "c29tZWh0aW5n",
+   *     "hash": "3FC9B689459D738F8C88A3A48AA9E33542016B7A4052E001AAA536FCA74813CB"
+   *   }
+   *  }
+   * @apiSuccess {String} apiVersion Version of the response layout (e.g., "1.0").
+   * @apiParam {Object} data Request payload.
+   * @apiSuccess {boolean} data.isIdentical true if the hash equals the hashed base64-string
+   * @apiSuccessExample {json} Success-Response
+   *  {
+   *  "apiVersion": "1.0",
+   *     "data": {
+   *       "isIdentical": false
+   *     }
+   *   }
    */
   router.post("/workflowitem.validateDocument", (req: AuthenticatedRequest, res) => {
     validateDocument(multichainClient, req)
