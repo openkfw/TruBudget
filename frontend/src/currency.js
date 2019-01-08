b6aacaa075b783c76f6e1f205ed874dcdb2bb48d
Mathias Höld <mathias.hoeld@accenture.com>
2019-01-08 17:20:57 +0100

    ui: Add currency for Burkina Faso (CFA franc)
    Add currency CAF to frontend and add e2e-tests for currencies in project
    overview

--- frontend/src/currency.js
+++ frontend/src/currency.js
@@ -1,7 +1,11 @@
+// These are the currencies that are available in the frontend
+// If there is a new currency added, please make sure to also add it to the list of currencies in the end-to-end test
+// See ../e2e-test/currencies_spec.js
 const currencies = {
   EUR: { symbol: "€", format: "%v %s" },
   USD: { symbol: "$", format: "%s %v" },
   BRL: { symbol: "R$", format: "%s %v" },
-}
+  XOF: { symbol: "CFA", format: "%s %v" }
+};
 
 export default currencies;
