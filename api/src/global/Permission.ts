9ed4159ba41c3ced3411579b9778b6415ed61b56
stefan.ziffer <stefan.ziffer@accenture.com>
2019-01-29 09:43:50 +0100

    api: Remove new functions from old Permissions file

--- api/src/global/Permission.ts
+++ api/src/global/Permission.ts
@@ -12,10 +12,11 @@ export function isAllowedToSee(permissions: Permissions, actingUser: User): bool
   const hasPermission = userIntents.includes(allowedIntent);
   return hasPermission;
 }
+
 export function isAllowedToGrant(permissions: Permissions, actingUser: User): boolean {
-  const allowedIntents: Intent[] = ["global.grantPermission"];
+  const allowedIntent: Intent = "global.grantPermission";
   const userIntents = getAllowedIntents(userIdentities(actingUser), permissions);
-  const hasPermission = allowedIntents.some(allowedIntent => userIntents.includes(allowedIntent));
+  const hasPermission = userIntents.includes(allowedIntent);
   return hasPermission;
 }
 
