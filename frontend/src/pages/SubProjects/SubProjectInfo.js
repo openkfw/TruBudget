266f2c4645f62200d6011ad6ca187f2546405242
Kevin Bader <kevin.bader@accenture.com>
2019-03-13 20:08:42 +0100

    ui: Fix display of subproject additional data

--- frontend/src/pages/SubProjects/SubProjectInfo.js
+++ frontend/src/pages/SubProjects/SubProjectInfo.js
@@ -53,8 +53,8 @@ const SubProjectInfo = ({ subProjects, idForInfo, isSubProjectAdditionalDataShow
               data-test="workflowitemInfoDisplayName"
               primary={""}
               secondary={
-                subProjectForInfo && !subProjectForInfo.data.additionalData ? (
-                  <pre>{JSON.stringify((JSON.parse(subProjectForInfo.data.additionalData), null, `\t`))}</pre>
+                subProjectForInfo && subProjectForInfo.data.additionalData ? (
+                  <pre>{JSON.stringify(subProjectForInfo.data.additionalData, null, `\t`)}</pre>
                 ) : (
                   "No fields are added to this Subproject"
                 )
