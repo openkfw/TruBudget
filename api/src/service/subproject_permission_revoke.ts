3dcd36a9436da3f300199ea4f901ab8ba259fc9c
Mathias Höld <mathias.hoeld@accenture.com>
2019-03-20 09:52:18 +0100

    api: implement service/subproject_permission_revoke

---
+++ api/src/service/subproject_permission_revoke.ts
@@ -0,0 +1,44 @@
+import Intent from "../authz/intents";
+import { Ctx } from "../lib/ctx";
+import * as Result from "../result";
+import * as Cache from "./cache2";
+import { ConnToken } from "./conn";
+import { Identity } from "./domain/organization/identity";
+import { ServiceUser } from "./domain/organization/service_user";
+import * as Project from "./domain/workflow/project";
+import * as Subproject from "./domain/workflow/subproject";
+import * as SubprojectPermissionRevoke from "./domain/workflow/subproject_permission_revoke";
+import { store } from "./store";
+
+export { RequestData } from "./domain/workflow/project_create";
+
+export async function revokeSubprojectPermission(
+  conn: ConnToken,
+  ctx: Ctx,
+  serviceUser: ServiceUser,
+  projectId: Project.Id,
+  subprojectId: Subproject.Id,
+  revokee: Identity,
+  intent: Intent,
+): Promise<void> {
+  const result = await Cache.withCache(conn, ctx, async cache =>
+    SubprojectPermissionRevoke.revokeSubprojectPermission(
+      ctx,
+      serviceUser,
+      projectId,
+      subprojectId,
+      revokee,
+      intent,
+      {
+        getSubproject: async (pId, spId) => {
+          return cache.getSubproject(pId, spId);
+        },
+      },
+    ),
+  );
+  if (Result.isErr(result)) return Promise.reject(result);
+
+  for (const event of result.newEvents) {
+    await store(conn, ctx, event);
+  }
+}
