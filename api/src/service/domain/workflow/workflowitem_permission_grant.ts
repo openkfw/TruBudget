b2e926bcc24f81bb776497b991b608e5aba38b47
stefan.ziffer <stefan.ziffer@accenture.com>
2019-03-20 10:39:42 +0100

    add workflowitem_permissions_revoke/grant to domain layer

---
+++ api/src/service/domain/workflow/workflowitem_permission_grant.ts
@@ -0,0 +1,65 @@
+import Intent from "../../../authz/intents";
+import { Ctx } from "../../../lib/ctx";
+import * as Result from "../../../result";
+import { BusinessEvent } from "../business_event";
+import { InvalidCommand } from "../errors/invalid_command";
+import { NotAuthorized } from "../errors/not_authorized";
+import { NotFound } from "../errors/not_found";
+import { Identity } from "../organization/identity";
+import { ServiceUser } from "../organization/service_user";
+import * as Project from "./project";
+import * as Subproject from "./subproject";
+import * as Workflowitem from "./workflowitem";
+
+import * as WorkflowitemPermissionGranted from "./workflowitem_permission_granted";
+
+interface Repository {
+  getWorkflowitem(
+    projectId: Project.Id,
+    subprojectId: Subproject.Id,
+    workflowitemId: Workflowitem.Id,
+  ): Promise<Result.Type<Workflowitem.Workflowitem>>;
+}
+
+export async function grantWorkflowitemPermission(
+  ctx: Ctx,
+  issuer: ServiceUser,
+  projectId: Project.Id,
+  subprojectId: Subproject.Id,
+  workflowitemId: Workflowitem.Id,
+  grantee: Identity,
+  intent: Intent,
+  repository: Repository,
+): Promise<Result.Type<{ newEvents: BusinessEvent[] }>> {
+  const workflowitem = await repository.getWorkflowitem(projectId, subprojectId, workflowitemId);
+
+  if (Result.isErr(workflowitem)) {
+    return new NotFound(ctx, "workflowitem", workflowitemId);
+  }
+
+  // Create the new event:
+  const permissionGranted = WorkflowitemPermissionGranted.createEvent(
+    ctx.source,
+    issuer.id,
+    projectId,
+    subprojectId,
+    workflowitemId,
+    intent,
+    grantee,
+  );
+
+  // Check authorization (if not root):
+  if (issuer.id !== "root") {
+    if (!Workflowitem.permits(workflowitem, issuer, ["workflowitem.intent.grantPermission"])) {
+      return new NotAuthorized(ctx, issuer.id, permissionGranted);
+    }
+  }
+
+  // Check that the new event is indeed valid:
+  const next = WorkflowitemPermissionGranted.apply(ctx, permissionGranted, workflowitem);
+  if (Result.isErr(next)) {
+    return new InvalidCommand(ctx, permissionGranted, [next]);
+  }
+
+  return { newEvents: [permissionGranted] };
+}
