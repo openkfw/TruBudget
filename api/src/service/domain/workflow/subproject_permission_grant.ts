48e30aba1108fd6dfd2a9091f32bf356d3e6d2d5
Mathias Höld <mathias.hoeld@accenture.com>
2019-03-20 09:06:43 +0100

    api: add subproject_permission_grant to domain layer

---
+++ api/src/service/domain/workflow/subproject_permission_grant.ts
@@ -0,0 +1,57 @@
+import Intent from "../../../authz/intents";
+import { Ctx } from "../../../lib/ctx";
+import * as Result from "../../../result";
+import { BusinessEvent } from "../business_event";
+import { InvalidCommand } from "../errors/invalid_command";
+import { NotAuthorized } from "../errors/not_authorized";
+import { NotFound } from "../errors/not_found";
+import { Identity } from "../organization/identity";
+import { ServiceUser } from "../organization/service_user";
+import * as Project from "./project";
+import * as Subproject from "./subproject";
+import * as SubprojectPermissionGranted from "./subproject_permission_granted";
+
+interface Repository {
+  getSubproject(subprojectId: Subproject.Id): Promise<Result.Type<Subproject.Subproject>>;
+}
+
+export async function grantSubprojectPermission(
+  ctx: Ctx,
+  issuer: ServiceUser,
+  projectId: Project.Id,
+  subprojectId: Subproject.Id,
+  grantee: Identity,
+  intent: Intent,
+  repository: Repository,
+): Promise<Result.Type<{ newEvents: BusinessEvent[] }>> {
+  const subproject = await repository.getSubproject(subprojectId);
+
+  if (Result.isErr(subproject)) {
+    return new NotFound(ctx, "project", subprojectId);
+  }
+
+  // Create the new event:
+  const permissionGranted = SubprojectPermissionGranted.createEvent(
+    ctx.source,
+    issuer.id,
+    projectId,
+    subprojectId,
+    intent,
+    grantee,
+  );
+
+  // Check authorization (if not root):
+  if (issuer.id !== "root") {
+    if (!Subproject.permits(subproject, issuer, ["subproject.intent.grantPermission"])) {
+      return new NotAuthorized(ctx, issuer.id, permissionGranted);
+    }
+  }
+
+  // Check that the new event is indeed valid:
+  const next = SubprojectPermissionGranted.apply(ctx, permissionGranted, subproject);
+  if (Result.isErr(next)) {
+    return new InvalidCommand(ctx, permissionGranted, [next]);
+  }
+
+  return { newEvents: [permissionGranted] };
+}
