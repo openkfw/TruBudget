c8664e5f23ab524edd5962a279f947e8df9e32e2
Philip Pai <philip.pai.mail@gmail.com>
2019-03-25 13:40:23 +0100

    update docker image repository

--- scripts/master/start-master-node.sh
+++ scripts/master/start-master-node.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-docker-compose -f docker-compose/master/master-node.yml down 
+docker-compose -f docker-compose/master/master-node.yml down
 docker rm $(docker ps -q --filter status=exited --filter label=com.docker.compose.project=trubudget)
 
-echo "Login to Docker-Registry(index.docker.io)"
-cat DOCKER_REGISTRY_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin index.docker.io
-
-docker-compose -f docker-compose/master/master-node.yml pull 
-docker-compose -p trubudget -f docker-compose/master/master-node.yml up --build 
+docker-compose -f docker-compose/master/master-node.yml pull
+docker-compose -p trubudget -f docker-compose/master/master-node.yml up --build
--- scripts/master/start-slave-node.sh
+++ scripts/master/start-slave-node.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-docker-compose -f docker-compose/master/slave-node.yml down 
+docker-compose -f docker-compose/master/slave-node.yml down
 docker rm $(docker ps -q --filter status=exited --filter label=com.docker.compose.project=trubudget)
 
-echo "Login to Docker-Registry(index.docker.io)"
-cat DOCKER_REGISTRY_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin index.docker.io
-
-docker-compose -f docker-compose/master/slave-node.yml pull 
+docker-compose -f docker-compose/master/slave-node.yml pull
 docker-compose -p trubudget -f docker-compose/master/slave-node.yml up --build
