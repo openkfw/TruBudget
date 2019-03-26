aa257b115e1069dd3819cf4971f27689fd5cf3e0
Philip Pai <philip.pai.mail@gmail.com>
2019-03-26 14:38:40 +0100

    make publish-docker-images.sh executable

--- scripts/pipeline/publish-docker-images.sh
+++ scripts/pipeline/publish-docker-images.sh
@@ -3,23 +3,28 @@ set -ev
 
 export BUILDTIMESTAMP=$(date -Iseconds)
 export TAG=trubudget/$PROJECT_NAME:$TRAVIS_BRANCH
-export TAG_BUILD=trubudget/$PROJECT_NAME:$TRAVIS_BUILD_ID
+export TAG_BUILD_PRIVATE=$PRIVATE_REGISTRY_BASE/trubudget/$PROJECT_NAME:t_$TRAVIS_BUILD_ID
 export TAG_LATEST=trubudget/$PROJECT_NAME:latest
 
 docker build --build-arg BUILDTIMESTAMP=$BUILDTIMESTAMP --build-arg CI_COMMIT_SHA=$TRAVIS_COMMIT --tag $TAG -f Dockerfile .
 
+echo "$PRIVATE_REGISTRY_PASSWORD" | docker login -u "$PRIVATE_REGISTRY_USERNAME" --password-stdin "$PRIVATE_REGISTRY"
+docker tag "$TAG" "$TAG_BUILD_PRIVATE"
+echo "Pushing [private]/trubudget/$PROJECT_NAME:$TRAVIS_BUILD_ID"
+docker push "$TAG_BUILD_PRIVATE" >/dev/null 2>&1
+
 if [ "$TRAVIS_BRANCH" = "master" ];
 then
+  echo "Enter Master Path"
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker push $TAG
-else
-  echo "$PRIVATE_REGISTRY_PASSWORD" | docker login -u "$PRIVATE_REGISTRY_USERNAME" --password-stdin "$PRIVATE_REGISTRY"
-  docker tag "$TAG" "$TAG_BUILD"
-  docker push "$TAG_BUILD"
 fi
 
 if [ -n "$TRAVIS_TAG" ];
 then
+  echo "Enter TAG Path"
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker tag "$TAG" "$TAG_LATEST"
+  docker push $TAG
   docker push "$TAG_LATEST"
 fi
