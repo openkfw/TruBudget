abe74a2ab52eec20c98681fd5b33a18e967bf7c0
Philip Pai <philip.pai.mail@gmail.com>
2019-04-04 16:07:09 +0200

    provisioning: add status code 400 to the retry list

--- provisioning/src/lib.js
+++ provisioning/src/lib.js
@@ -16,17 +16,24 @@ async function withRetry(cb, maxTimes = 24, timeoutMs = 20000) {
       (err.response && err.response.status === 500) ||
       (err.response && err.response.status === 500) ||
       (err.response && err.response.status === 404) ||
+      (err.response && err.response.status === 400) ||
       (!err.response && err.code === "ECONNREFUSED") ||
       (!err.response && err.code === "ECONNABORTED")
     ) {
       console.log(
-        `Server Error (${err.message}), retry in ${timeoutMs /
-          1000} seconds`
+        `Server Error (${err.message}), retry in ${timeoutMs / 1000} seconds`
       );
       await timeout(timeoutMs);
       return await withRetry(cb, --maxTimes);
-    } else if (err.response && err.response.status >= 400 && err.response.status < 500) {
-      console.log(`The request had no effect: a precondition was not fulfilled:`, err.response.data);
+    } else if (
+      err.response &&
+      err.response.status >= 400 &&
+      err.response.status < 500
+    ) {
+      console.log(
+        `The request had no effect: a precondition was not fulfilled:`,
+        err.response.data
+      );
     } else {
       console.error(err);
       process.exit(1);
