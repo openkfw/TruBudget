1146cc9b6cd0bdcf704332576ee539c925a768d2
Mathias Höld <mathias.hoeld@accenture.com>
2019-04-08 15:43:45 +0200

    ui: Enable in-line edit of projected budgets
    If the user wants to edit an already added projected budget, he/she can
    now press the edit button next to the projected budget and enter a new
    value.

--- frontend/src/pages/Common/Budget2.js
+++ frontend/src/pages/Common/Budget2.js
@@ -1,26 +1,28 @@
-import React from "react";
+import Button from "@material-ui/core/Button";
+import MenuItem from "@material-ui/core/MenuItem";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import DropDown from "./NewDropdown";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import React from "react";
 
-import MenuItem from "@material-ui/core/MenuItem";
+import DoneIcon from "@material-ui/icons/Check";
+import { fromAmountString, getCurrencies, toAmountString } from "../../helper";
 import strings from "../../localizeStrings";
-
-import { toAmountString, fromAmountString, getCurrencies } from "../../helper";
+import DropDown from "./NewDropdown";
 
 export default class Budget extends React.Component {
   state = {
     budgetAmount: "",
+    budgetAmountEdit: "",
     organization: "",
     currency: "",
-    edit: false
+    edit: false,
+    editIndex: -1
   };
   getMenuItems(currencies) {
     return currencies.map((currency, index) => {
@@ -36,26 +38,17 @@ export default class Budget extends React.Component {
     this.props.storeDeletedProjectedBudget(this.addBudget(deletedProjectedBudgets, budgetToDelete));
   }
 
-  deleteBudget(budgets, budgetToDelete) {
-    for (const index in budgets) {
-      if (
-        budgets[index].organization === budgetToDelete.organization &&
-        budgets[index].currencyCode === budgetToDelete.currencyCode
-      ) {
-        budgets.splice(index, 1);
-      }
-    }
-    return budgets;
-  }
+  editProjectedBudget(budgets, budget, budgetAmountEdit) {
+    const updateIndex = budgets.findIndex(
+      x => x.organization === budget.organization && x.currencyCode === budget.currencyCode
+    );
 
-  editProjectedBudget(budgets, budget) {
+    budgets[updateIndex].value = budgetAmountEdit;
+    this.props.storeProjectedBudget(budgets);
     this.setState({
-      edit: true,
-      organization: budget.organization,
-      currency: budget.currencyCode,
-      budgetAmount: budget.value
+      edit: false,
+      editIndex: -1
     });
-    this.deleteBudget(budgets, budget);
   }
 
   addBudget(budgets, budgetToAdd) {
@@ -80,94 +73,115 @@ export default class Budget extends React.Component {
             {projectedBudgets.map((budget, i) => (
               <TableRow key={`pb-row-${budget.organization}-${budget.value}-${i}`}>
                 <TableCell>{budget.organization}</TableCell>
-                <TableCell align="right">{toAmountString(budget.value, budget.currencyCode)}</TableCell>
                 <TableCell align="right">
-                  {true ? (
-                    <Button aria-label="Edit" onClick={() => this.editProjectedBudget(projectedBudgets, budget)}>
+                  {this.state.edit && this.state.editIndex === i ? (
+                    <TextField
+                      label={strings.common.projectedBudget}
+                      value={this.state.budgetAmountEdit}
+                      onChange={e => this.setState({ budgetAmountEdit: e.target.value })}
+                      type="text"
+                      aria-label="projectedBudgetAmountEdit"
+                      id="amountedit"
+                    />
+                  ) : (
+                    toAmountString(budget.value, budget.currencyCode)
+                  )}
+                </TableCell>
+                <TableCell align="right">
+                  {this.state.edit && this.state.editIndex === i ? (
+                    <Button
+                      aria-label="Done"
+                      onClick={() => this.editProjectedBudget(projectedBudgets, budget, this.state.budgetAmountEdit)}
+                    >
+                      <DoneIcon />
+                    </Button>
+                  ) : (
+                    <Button
+                      aria-label="Edit"
+                      onClick={() => this.setState({ editIndex: i, edit: true, budgetAmountEdit: budget.value })}
+                      disabled={this.state.edit}
+                    >
                       <EditIcon />
                     </Button>
-                  ) : null}
-                  {true ? (
-                    <Button
-                      aria-label="Delete"
-                      onClick={() => this.deleteBudgetFromList(projectedBudgets, deletedProjectedBudgets, budget)}
-                    >
-                      <DeleteIcon />
-                    </Button>
-                  ) : null}
+                  )}
+                  <Button
+                    aria-label="Delete"
+                    onClick={() => this.deleteBudgetFromList(projectedBudgets, deletedProjectedBudgets, budget)}
+                  >
+                    <DeleteIcon />
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
-            {true ? (
-              <TableRow key={`pb-row-add`}>
-                <TableCell>
+            <TableRow key={`pb-row-add`}>
+              <TableCell>
+                <TextField
+                  label={strings.users.organization}
+                  value={this.state.organization}
+                  onChange={e => this.setState({ organization: e.target.value })}
+                  type="text"
+                  aria-label="organization"
+                  id="organizationinput"
+                  disabled={this.state.edit}
+                />
+              </TableCell>
+              <TableCell align="right">
+                <div style={{ display: "flex" }}>
+                  <DropDown
+                    style={{ minWidth: 200, marginRight: "16px" }}
+                    value={this.state.currency}
+                    floatingLabel={strings.project.project_currency}
+                    onChange={e => this.setState({ currency: e })}
+                    id="currencies"
+                    disabled={this.state.edit}
+                  >
+                    {this.getMenuItems(currencies)}
+                  </DropDown>
                   <TextField
-                    label={strings.users.organization}
-                    value={this.state.organization}
-                    onChange={e => this.setState({ organization: e.target.value })}
+                    label={strings.common.projectedBudget}
+                    value={this.state.budgetAmount}
+                    onChange={v => {
+                      if (/^[0-9,.-]*$/.test(v.target.value)) this.setState({ budgetAmount: v.target.value });
+                    }}
+                    onBlur={e => this.setState({ budgetAmount: toAmountString(e.target.value) })}
+                    onFocus={() => this.setState({ budgetAmount: fromAmountString(this.state.budgetAmount) })}
                     type="text"
-                    aria-label="organization"
-                    id="organizationinput"
-                    disabled={this.edit}
+                    multiline={false}
+                    aria-label="projectedbudget"
+                    id="projectedbudgetinput"
+                    style={{
+                      width: "60%"
+                    }}
                   />
-                </TableCell>
-                <TableCell align="right">
-                  <div style={{ display: "flex" }}>
-                    <DropDown
-                      style={{ minWidth: 200, marginRight: "16px" }}
-                      value={this.state.currency}
-                      floatingLabel={strings.project.project_currency}
-                      onChange={e => this.setState({ currency: e })}
-                      id="currencies"
-                    >
-                      {this.getMenuItems(currencies)}
-                    </DropDown>
-                    <TextField
-                      label={strings.common.projectedBudget}
-                      value={this.state.budgetAmount}
-                      onChange={v => {
-                        if (/^[0-9,.-]*$/.test(v.target.value)) this.setState({ budgetAmount: v.target.value });
-                      }}
-                      onBlur={e => this.setState({ budgetAmount: toAmountString(e.target.value) })}
-                      onFocus={() => this.setState({ budgetAmount: fromAmountString(this.state.budgetAmount) })}
-                      type="text"
-                      multiline={false}
-                      aria-label="projectedbudget"
-                      id="projectedbudgetinput"
-                      style={{
-                        width: "60%"
-                      }}
-                    />
-                  </div>
-                </TableCell>
-                <TableCell align="right">
-                  {
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      disabled={!this.state.budgetAmount || !this.state.currency || !this.state.organization}
-                      onClick={() => {
-                        this.setState({ edit: false });
-                        const projectedBudgetsCopy = projectedBudgets;
-                        projectedBudgetsCopy.push({
-                          value: fromAmountString(this.state.budgetAmount).toString(10),
-                          currencyCode: this.state.currency,
-                          organization: this.state.organization
-                        });
-                        storeProjectedBudget(projectedBudgetsCopy);
-                        this.setState({
-                          budgetAmount: "",
-                          organization: "",
-                          currency: ""
-                        });
-                      }}
-                    >
-                      {this.state.edit ? "Edit" : `${strings.common.add}`}
-                    </Button>
-                  }
-                </TableCell>
-              </TableRow>
-            ) : null}
+                </div>
+              </TableCell>
+              <TableCell align="right">
+                {
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    disabled={!this.state.budgetAmount || !this.state.currency || !this.state.organization}
+                    onClick={() => {
+                      this.setState({ edit: false });
+                      const projectedBudgetsCopy = projectedBudgets;
+                      projectedBudgetsCopy.push({
+                        value: fromAmountString(this.state.budgetAmount).toString(10),
+                        currencyCode: this.state.currency,
+                        organization: this.state.organization
+                      });
+                      storeProjectedBudget(projectedBudgetsCopy);
+                      this.setState({
+                        budgetAmount: "",
+                        organization: "",
+                        currency: ""
+                      });
+                    }}
+                  >
+                    {this.state.edit ? `${strings.common.edit}` : `${strings.common.add}`}
+                  </Button>
+                }
+              </TableCell>
+            </TableRow>
           </TableBody>
         </Table>
       </div>
