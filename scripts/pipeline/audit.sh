f069c35cfd56986b258aad6d7be2b79ccaf123bf
Philip Pai <philip.pai.mail@gmail.com>
2019-05-08 11:38:38 +0200

    fix audit job in case of npm audit not being able to retrieve results

--- scripts/pipeline/audit.sh
+++ scripts/pipeline/audit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep -q "$PROJECT_NAME/" || ["$TRAVIS_EVENT_TYPE" = 'cron'];
+if [[ "$TRAVIS_EVENT_TYPE" = "cron" ]] || (git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep -q "$PROJECT_NAME/");
 then
   node -e '
   const { exec } = require("child_process");
@@ -10,14 +10,14 @@ then
       console.error(err);
       process.exit(1);
     }
-    const {
-      metadata: { vulnerabilities }
-    } = JSON.parse(stdout);
 
-    if (vulnerabilities.high > 0) {
-      console.error("NPM Audit found high vulnerabilities:", vulnerabilities);
+    const result = JSON.parse(stdout);
+    if (result && result.metadata && result.metadata.vulnerabilities && result.metadata.vulnerabilities.high && result.metadata.vulnerabilities.high > 0) {
+      console.error("NPM Audit found high vulnerabilities:", result.metadata.vulnerabilities);
       process.exit(1);
     }
+
+    console.log("Everything OK. No high vulnerabilities found.");
   });';
 else
   echo "No file changes found in $PROJECT_NAME and not triggered by cron. Skipping audit.";
