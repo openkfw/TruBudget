5b18bc04150bdc4ba4db58b46f82b0239e3e3119
Stezido <stefan.ziffer@accenture.com>
2019-12-19 09:18:16 +0100

    e2e-tests: Separate logout tests
    Otherwise the test fails in CI

--- e2e-test/cypress/integration/login_spec.js
+++ e2e-test/cypress/integration/login_spec.js
@@ -1,62 +1,62 @@
 describe("Login", function() {
-  let routes;
+  let projectId, subprojectId;
+  const routes = {
+    overview: "projects",
+    users: "users",
+    notifications: "notifications",
+    nodes: "nodes",
+    projectDetails: `projects/${projectId}`,
+    subprojectDetails: `projects/${projectId}/${subprojectId}`,
+    notFound: "notfound"
+  };
 
   before(function() {
     cy.login();
     cy.createProject("p-login", "login test").then(({ id }) => {
-      const projectId = id;
+      projectId = id;
       cy.createSubproject(projectId, "sp-login").then(({ id }) => {
-        const subprojectId = id;
-        routes = [
-          "projects",
-          "users",
-          "notifications",
-          "nodes",
-          `projects/${projectId}`,
-          `projects/${projectId}/${subprojectId}`,
-          "notfound"
-        ];
+        subprojectId = id;
+        // Logout
+        localStorage.setItem("state", undefined);
       });
     });
   });
 
   beforeEach(function() {
-    cy.login();
     cy.visit(`/`);
   });
 
-  it(`Log in and out on every route`, function() {
-    cy.get("#logoutbutton")
-      .should("be.visible")
-      .click();
-    routes.forEach(route => {
-      // Login process
-      cy.get("#loginpage")
-        .should("be.visible")
-        .get("#username")
-        .type("mstein")
-        .should("have.value", "mstein")
-        .get("#password")
-        .type("test")
-        .should("have.value", "test")
-        .get("#loginbutton")
-        .click();
-      // Check if logged in correctly
-      cy.get("#logoutbutton").should("be.visible");
-      // Logout on specific route
-      cy.visit(`/${route}`);
-      cy.get("#logoutbutton")
-        .should("be.visible")
-        .click();
-      // Check if logged out correctly
-      cy.get("#loginpage").should("be.visible");
-    });
+  it(`Log in and out on overview page`, function() {
+    loginUi();
+    logout(routes.overview);
+  });
+
+  it(`Log in and out on users page`, function() {
+    loginUi();
+    logout(routes.users);
+  });
+  it(`Log in and out on notifications page`, function() {
+    loginUi();
+    logout(routes.notifications);
+  });
+  it(`Log in and out on nodes page`, function() {
+    loginUi();
+    logout(routes.nodes);
+  });
+  it(`Log in and out on projectDetails page`, function() {
+    loginUi();
+    logout(routes.projectDetails);
+  });
+  it(`Log in and out on subprojectDetails page`, function() {
+    loginUi();
+    logout(routes.subprojectDetails);
+  });
+  it(`Log in and out on a page that's not found`, function() {
+    loginUi();
+    logout(routes.notFound);
   });
 
   it("Reject wrong inputs", function() {
-    cy.get("#logoutbutton")
-      .should("be.visible")
-      .click();
     cy.get("#loginpage").should("be.visible");
     cy.get("#username")
       .should("be.visible")
@@ -69,3 +69,27 @@ describe("Login", function() {
     cy.get("#password-helper-text").should("be.visible");
   });
 });
+
+function logout(route) {
+  cy.visit(`/${route}`);
+  cy.get("#logoutbutton")
+    .should("be.visible")
+    .click();
+  // Check if logged out correctly
+  cy.get("#loginpage").should("be.visible");
+}
+
+function loginUi() {
+  cy.get("#loginpage")
+    .should("be.visible")
+    .get("#username")
+    .type("mstein")
+    .should("have.value", "mstein")
+    .get("#password")
+    .type("test")
+    .should("have.value", "test")
+    .get("#loginbutton")
+    .click();
+  // Check if logged in correctly
+  cy.get("#logoutbutton").should("be.visible");
+}
