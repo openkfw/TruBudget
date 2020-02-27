d1480ee9446111bb0313b12cc83450681ac2433a
daniel.arnauer <daniel.arnauer@accenture.com>
2020-02-27 11:05:29 +0100

    e2e-test: Add new test for timestamp

---
+++ e2e-test/cypress/integration/timestamp_spec.js
@@ -0,0 +1,53 @@
+const standardBudget = [
+  {
+    organization: "Test",
+    value: "12345",
+    currencyCode: "EUR"
+  }
+];
+
+describe("Timestamps", function() {
+  before(function() {
+    cy.login("mstein", "test");
+    cy.visit(`/projects`);
+    cy.createProject("p-timestamp", "project timestamp test", standardBudget);
+  });
+
+  beforeEach(function() {
+    cy.login("mstein", "test");
+    cy.visit(`/projects`);
+  });
+
+  it("Check timestamp in notifications", function() {
+    cy.get("[data-test*=project-view-button]")
+      .last()
+      .click();
+    cy.get("[data-test=assignee-selection]").click();
+    cy.get("[data-test=assignee-list]")
+      .should("be.visible")
+      .find("[value=thouse]")
+      .click()
+      .get("[data-test=confirmation-dialog-confirm]")
+      .click();
+    cy.login("thouse", "test");
+    cy.visit("/notifications");
+    cy.get("[data-test=notification-list]")
+      .last()
+      .should("be.visible");
+    cy.get("[data-test=dateOfNotification-0]").then(element => {
+      const text = element.text();
+      expect(text).to.match(/[0-9][0-9].[0-9][0-9].[0-9][0-9][0-9][0-9]/);
+    });
+  });
+
+  it("Check timestamp in history", function() {
+    cy.get("[data-test*=project-view-button]")
+      .last()
+      .click();
+    cy.get("[data-test=project-history-button]").click();
+    cy.get("[data-test=history-item-0]").then(element => {
+      const text = element.text();
+      expect(text).to.match(/[0-9][0-9].[0-9][0-9].[0-9][0-9][0-9][0-9]/);
+    });
+  });
+});
