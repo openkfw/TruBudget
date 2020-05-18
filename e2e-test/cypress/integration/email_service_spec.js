88ed36e1e107079578cd3f337f2e7b40322ba5cb
Stezido <stefan.ziffer@accenture.com>
2020-05-18 10:47:01 +0200

    Remove email service e2e-test

--- e2e-test/cypress/integration/email_service_spec.js
+++ e2e-test/cypress/integration/email_service_spec.js
@@ -1,8 +1,4 @@
 describe("Email Service test", function() {
-  before(() => {});
-
-  beforeEach(function() {});
-
   it("If the email server is not available, user cannot change email address", function() {
     cy.login();
     cy.visit("/projects/");
@@ -14,27 +10,6 @@ describe("Email Service test", function() {
     cy.get("[data-test=show-user-profile]").click();
     cy.get("[data-test=user-profile-dialog").should("be.visible");
     cy.get("[data-test=email-address-input").should("not.be.visible");
-
-    // Close
-    cy.get("[data-test=close-user-profile]").click();
-    cy.get("[data-test=side-navigation").should("be.visible");
-  });
-
-  it("If the email server is available, user can change email address", function() {
-    cy.server();
-    cy.route("**/readiness", [200]);
-    cy.route("**/email/readiness", [200]);
-    cy.login();
-    cy.visit("/projects");
-
-    // Open side navigation
-    cy.get("[data-test=openSideNavbar]").click();
-    cy.get("[data-test=side-navigation").should("be.visible");
-
-    // Open user profile
-    cy.get("[data-test=show-user-profile]").click();
-    cy.get("[data-test=user-profile-dialog").should("be.visible");
-    cy.get("[data-test=email-address-input").should("be.visible");
 
     // Close
     cy.get("[data-test=close-user-profile]").click();
