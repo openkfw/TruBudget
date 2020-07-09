338a726511e1fd1ea998f045b61984881358876a
Stefan Ziffer <stefan.ziffer@accenture.com>
2020-07-09 12:07:01 +0200

    e2e-test: Refactor documents tests
    Wait for api calls if something is updated before take the next action

--- e2e-test/cypress/integration/documents_spec.js
+++ e2e-test/cypress/integration/documents_spec.js
@@ -3,13 +3,15 @@ import "cypress-file-upload";
 let projectId;
 let subprojectId;
 let workflowitemId;
+let baseUrl, apiRoute;
 // Actual file in fixture folder
 const fileName = "documents_test.json";
 
 describe("Attaching a document to a workflowitem.", function() {
   before(() => {
+    baseUrl = Cypress.env("API_BASE_URL") || `${Cypress.config("baseUrl")}/test`;
+    apiRoute = baseUrl.toLowerCase().includes("test") ? "/test/api" : "/api";
     cy.login();
-
     cy.createProject("documents test project", "workflowitem documents test", [])
       .then(({ id }) => {
         projectId = id;
@@ -32,13 +34,19 @@ describe("Attaching a document to a workflowitem.", function() {
 
   const uploadDocument = (fileDisplayName, fileName) => {
     // open edit dialog:
-    cy.get("div[title=Edit] > button").click();
+    cy.get("div[title=Edit] > button")
+      .should("be.visible")
+      .click();
 
     // click "next" button:
-    cy.get("[data-test=next]").click();
+    cy.get("[data-test=next]")
+      .should("be.visible")
+      .click();
 
     // enter the file name:
-    cy.get("#documentnameinput").type(fileDisplayName);
+    cy.get("#documentnameinput")
+      .should("be.visible")
+      .type(fileDisplayName);
 
     // "upload" the file:
     cy.fixture(fileName).then(fileContent => {
@@ -52,15 +60,28 @@ describe("Attaching a document to a workflowitem.", function() {
 
   it("A document can be validated.", function() {
     const fileDisplayName = "Validation_Test";
+    cy.server();
+    cy.route("POST", apiRoute + "/workflowitem.update*").as("update");
+    cy.route("GET", apiRoute + "/subproject.viewDetails*").as("viewDetails");
+    cy.route("POST", apiRoute + "/workflowitem.validate*").as("validate");
+
     uploadDocument(fileDisplayName, fileName);
     // submit and close the dialog:
-    cy.get("[data-test=submit]").click();
+    cy.get("[data-test=submit]")
+      .should("be.visible")
+      .click();
 
     // open the info dialog window:
-    cy.get(`[data-test^='workflowitem-info-button-${workflowitemId}']`).click();
+    cy.wait("@update")
+      .wait("@viewDetails")
+      .get(`[data-test^='workflowitem-info-button-${workflowitemId}']`)
+      .should("be.visible")
+      .click();
 
     // go to the documents tab:
-    cy.get("[data-test=workflowitem-documents-tab]").click();
+    cy.get("[data-test=workflowitem-documents-tab]")
+      .should("be.visible")
+      .click();
 
     // upload the same file, for validation:
     cy.fixture(fileName).then(fileContent => {
@@ -71,23 +92,37 @@ describe("Attaching a document to a workflowitem.", function() {
     });
 
     // make sure the validation was successful:
-    cy.get("[data-test=workflowitem-documents-tab]").click();
-    cy.get(`button[label="Validated!"] > span`).should("contain", "OK");
+    cy.wait("@validate")
+      .get(`button[label="Validated!"] > span`)
+      .should("contain", "OK");
   });
 
   it("Validation of wrong document fails.", function() {
     const fileDisplayName = "Wrong_Document_Test";
+    cy.server();
+    cy.route("POST", apiRoute + "/workflowitem.update*").as("update");
+    cy.route("GET", apiRoute + "/subproject.viewDetails*").as("viewDetails");
+    cy.route("POST", apiRoute + "/workflowitem.validate*").as("validate");
+
     uploadDocument(fileDisplayName, fileName);
     // submit and close the dialog:
-    cy.get("[data-test=submit]").click();
+    cy.get("[data-test=submit]")
+      .should("be.visible")
+      .click();
 
     // open the info dialog window:
-    cy.get(`[data-test^='workflowitem-info-button-${workflowitemId}']`).click();
+    cy.wait("@update")
+      .wait("@viewDetails")
+      .get(`[data-test^='workflowitem-info-button-${workflowitemId}']`)
+      .should("be.visible")
+      .click();
 
     // go to the documents tab:
-    cy.get("[data-test=workflowitem-documents-tab]").click();
+    cy.get("[data-test=workflowitem-documents-tab]")
+      .should("be.visible")
+      .click();
 
-    // make sure the validation fails with the wrong document
+    // upload wrong document
     const wrongFileName = "testdata.json";
     cy.fixture(wrongFileName).then(fileContent => {
       cy.get("#docvalidation").upload(
@@ -97,7 +132,8 @@ describe("Attaching a document to a workflowitem.", function() {
     });
 
     // make sure the validation was not successful:
-    cy.get("[data-test=workflowitem-documents-tab]").click();
-    cy.get(`button[label="Changed!"] > span`).should("contain", "Not OK");
+    cy.wait("@validate")
+      .get(`button[label="Changed!"] > span`)
+      .should("contain", "Not OK");
   });
 });
