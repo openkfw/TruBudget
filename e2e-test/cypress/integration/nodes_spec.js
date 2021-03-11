43c7d418c2f00ed275addd38b88e344bddecb5fb
daniel-arnauer <daniel.arnauer@accenture.com>
2021-03-11 11:56:07 +0100

    e2e-test: Update nodes page tests

--- e2e-test/cypress/integration/nodes_spec.js
+++ e2e-test/cypress/integration/nodes_spec.js
@@ -1,29 +1,29 @@
-describe("Nodes Page", function() {
-  const organization = "KfW";
+describe("Nodes Page", function () {
   const firstNode = {
+    organization: "KfW",
     accessType: "admin",
-    connectionStatus: "Connection"
   };
 
-  beforeEach(function() {
+  beforeEach(function () {
     cy.login();
     cy.visit(`/nodes`);
   });
 
-  it("Show nodes table", function() {
-    cy.get("[data-test=nodes-table]").should("be.visible");
+  it("Show approved nodes table", function () {
+    cy.get("[data-test=approved-nodes-table]").should("be.visible");
   });
 
-  it("Show node connection status", function() {
-    cy.get("#tableTitle").should("be.visible");
-    cy.get("[data-test=nodes-table] table tbody tr").should("have.length.above", 0);
-    cy.get("[data-test=nodes-table] table tbody tr")
-      .eq(0)
-      .then($node => {
-        expect($node.find("th")).to.contains.text(organization);
-        const nodeCount = parseInt($node.find("td").eq(0).text().trim(), 10);
-        expect(nodeCount).to.be.greaterThan(0);
-        expect($node.find("td").eq(1)).to.contains.text(`${firstNode.accessType} (${firstNode.connectionStatus})`);
-      });
+  it("Show declined nodes table", function () {
+    cy.get("[data-test=declined-nodes-tab]").should("be.visible").click();
+    cy.get("[data-test=declined-nodes-table]").should("be.visible");
+
+  });
+
+  it("Show node connection status", function () {
+    cy.get("[data-test=approved-nodes-table-body]").should("have.length.above", 0);
+    cy.get(`[data-test=open-entry-${firstNode.organization}] `).should("be.visible").click()
+    cy.get(`[data-test=status-${firstNode.organization}]`)
+      .should("be.visible")
+      .should('have.css', 'background-color', 'rgb(0, 128, 0)');
   });
 });
