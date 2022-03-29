2243b479eddbf1fbef1fa23a275d37406febaeed
Moldovan, Georgia <georgia.moldovan@accenture.com>
2022-03-29 11:19:05 +0200

    e2e-test: Fix tests related to storage service

--- e2e-test/cypress/integration/status_spec.js
+++ e2e-test/cypress/integration/status_spec.js
@@ -1,4 +1,4 @@
-describe("Component Versions", function() {
+describe("Component Versions", function () {
   let exportUrl, apiBaseUrl, apiUrl;
   before(() => {
     apiBaseUrl = Cypress.env("API_BASE_URL") || `${Cypress.config("baseUrl")}/test`;
@@ -6,16 +6,16 @@ describe("Component Versions", function() {
     exportUrl = Cypress.env("EXPORT_SERVICE_BASE_URL") || `${Cypress.config("baseUrl")}/test/api/export/xlsx`;
   });
 
-  it("Shows status list", function() {
+  it("Shows status list", function () {
     cy.login();
     cy.visit(`/status`);
     cy.get("[data-test=status-table-body]")
       .should("be.visible")
       .children()
-      .should("have.length", 4);
+      .should("have.length", 5);
   });
 
-  it("Shows connection of export-service", function() {
+  it("Shows connection of export-service", function () {
     cy.intercept(exportUrl + "/readiness*").as("isExportReady");
     cy.visit("/");
     cy.wait("@isExportReady").then(() => loginUi());
@@ -25,7 +25,7 @@ describe("Component Versions", function() {
       .should("be.visible");
   });
 
-  it("Shows versions of basic services (frontend,api,blockchain,multichain) correctly", function() {
+  it("Shows versions of basic services (frontend,api,blockchain,multichain) correctly", function () {
     cy.intercept(apiUrl + "/version").as("fetchVersions");
     cy.login();
     cy.visit(`/status`);
