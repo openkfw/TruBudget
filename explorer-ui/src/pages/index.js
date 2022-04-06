4ee168031bbfa3bb8e3bdda667c71c4b0959a957
Daniel Arnauer <daniel.arnauer@accenture.com>
2022-04-06 11:06:05 +0200

    Init DataTable

--- explorer-ui/src/pages/index.js
+++ explorer-ui/src/pages/index.js
@@ -1,105 +1,32 @@
-import Head from 'next/head';
-import { Box, Container, Grid } from '@mui/material';
-import { Budget } from '../components/dashboard/budget';
-import { LatestOrders } from '../components/dashboard/latest-orders';
-import { LatestProducts } from '../components/dashboard/latest-products';
-import { Sales } from '../components/dashboard/sales';
-import { TasksProgress } from '../components/dashboard/tasks-progress';
-import { TotalCustomers } from '../components/dashboard/total-customers';
-import { TotalProfit } from '../components/dashboard/total-profit';
-import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
-import { DashboardLayout } from '../components/dashboard-layout';
+import Head from "next/head";
+import { Box, Container, Grid } from "@mui/material";
+import { Budget } from "../components/dashboard/budget";
+import { LatestOrders } from "../components/dashboard/latest-orders";
+import { LatestProducts } from "../components/dashboard/latest-products";
+import { Sales } from "../components/dashboard/sales";
+import { TasksProgress } from "../components/dashboard/tasks-progress";
+import { TotalCustomers } from "../components/dashboard/total-customers";
+import { TotalProfit } from "../components/dashboard/total-profit";
+import { TrafficByDevice } from "../components/dashboard/traffic-by-device";
+import { DashboardLayout } from "../components/dashboard-layout";
+import { DataTable } from "../components/transaction/DataTable";
 
 const Dashboard = () => (
   <>
     <Head>
-      <title>
-        Dashboard | Material Kit
-      </title>
+      <title>Dashboard | Material Kit</title>
     </Head>
     <Box
       component="main"
       sx={{
         flexGrow: 1,
-        py: 8
+        py: 8,
       }}
     >
       <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <Budget />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalCustomers />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TasksProgress />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalProfit sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <Sales />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <TrafficByDevice sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <LatestProducts sx={{ height: '100%' }} />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <LatestOrders />
+        <Grid container spacing={3}>
+          <Grid item lg={12} md={12} xl={12} xs={12}>
+            <DataTable />
           </Grid>
         </Grid>
       </Container>
@@ -107,10 +34,6 @@ const Dashboard = () => (
   </>
 );
 
-Dashboard.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+Dashboard.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Dashboard;
