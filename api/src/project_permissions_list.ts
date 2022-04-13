eb2830c62f87c17ea4d65088f1f5a8770a8b9a67
Mayr Martin <22145802+mayrmartin@users.noreply.github.com>
2022-04-13 14:05:10 +0200

    - define what exposable permissions are (type-save) - let api return only exposable permissions

--- api/src/project_permissions_list.ts
+++ api/src/project_permissions_list.ts
@@ -1,14 +1,13 @@
 import { FastifyInstance, RequestGenericInterface } from "fastify";
 import { VError } from "verror";
-
+import { AuthenticatedRequest } from "./httpd/lib";
 import { toHttpError } from "./http_errors";
 import * as NotAuthenticated from "./http_errors/not_authenticated";
-import { AuthenticatedRequest } from "./httpd/lib";
 import { Ctx } from "./lib/ctx";
 import { isNonemptyString } from "./lib/validation";
 import * as Result from "./result";
 import { ServiceUser } from "./service/domain/organization/service_user";
-import { filterPermissions, Permissions } from "./service/domain/permissions";
+import { getExposablePermissions, Permissions } from "./service/domain/permissions";
 
 function mkSwaggerSchema(server: FastifyInstance) {
   return {
@@ -100,8 +99,9 @@ export function addHttpHandler(server: FastifyInstance, urlPrefix: string, servi
           throw new VError(projectPermissions, "project.intent.listPermissions failed");
         }
 
-        // TODO use an exposedPermissions interface instead of a filter function
-        const filteredProjectPermissions = filterPermissions(projectPermissions, ["project.close"]);
+        const filteredProjectPermissions = getExposablePermissions(projectPermissions, [
+          "project.close",
+        ]);
 
         const code = 200;
         const body = {
--- api/src/service/domain/permissions.ts
+++ api/src/service/domain/permissions.ts
@@ -1,18 +1,22 @@
 import Joi = require("joi");
-
-import Intent, { allIntents } from "../../authz/intents";
+import Intent, { allIntents, ExposableIntent } from "../../authz/intents";
 import { Identity } from "./organization/identity";
 
 export type Permissions = { [key in Intent]?: Identity[] };
+export type ExposablePermissions = { [key in ExposableIntent]?: Identity[] };
 
 export const permissionsSchema = Joi.object().pattern(
   Joi.valid(allIntents),
   Joi.array().items(Joi.string()),
 );
 
-export const filterPermissions = (permissions: Permissions, filter: string[]): Permissions => {
+export const getExposablePermissions = (
+  permissions: Permissions,
+  filter: Array<"project.close" | "subproject.close" | "workflowitem.close">,
+): ExposablePermissions => {
+  const filterablePermissions = filter as string[];
   return Object.keys(permissions).reduce((filteredPerm, intent) => {
-    if (!filter.includes(intent)) {
+    if (!filterablePermissions.includes(intent)) {
       filteredPerm[intent] = permissions[intent];
     }
     return filteredPerm;
