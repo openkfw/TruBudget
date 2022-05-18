72ca677c956dbd448bf1c985393375989f098c64
Daniel Arnauer <daniel.arnauer@accenture.com>
2022-05-18 11:24:18 +0200

    UI: Format time to created-date

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -102,7 +102,7 @@ export const validateLanguagePattern = amount => {
 export const numberSignsRegex = /^[0-9,.-]*$/;
 
 export const unixTsToString = ts => {
-  let dateString = dayjs.unix(ts).format("D MMM YYYY");
+  let dateString = dayjs.unix(ts).format("D MMM YYYY HH:mm");
   return dateString;
 };
 
