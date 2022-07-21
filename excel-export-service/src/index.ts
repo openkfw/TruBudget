fcb7bfe6193ac5992ecda80f919939a7a0e32eef
Daniel Arnauer <daniel.arnauer@accenture.com>
2022-07-21 13:04:20 +0200

    Update excel npm packages

--- excel-export-service/src/index.ts
+++ excel-export-service/src/index.ts
@@ -1,14 +1,14 @@
-import axios, {AxiosRequestTransformer} from "axios";
+import axios, { AxiosRequestTransformer } from "axios";
 import * as cors from "cors";
 import * as express from "express";
-import {createPinoExpressLogger} from "trubudget-logging-service";
+import { createPinoExpressLogger } from "trubudget-logging-service";
 import * as URL from "url";
-import {getApiReadiness, getApiVersion} from "./api";
-import {config} from "./config";
-import {writeXLSX} from "./excel";
-import strings, {languages} from "./localizeStrings";
+import { getApiReadiness, getApiVersion } from "./api";
+import { config } from "./config";
+import { writeXLSX } from "./excel";
+import strings, { languages } from "./localizeStrings";
 import logger from "./logger";
-import {CustomExpressRequest, CustomExpressResponse} from "./types";
+import { CustomExpressRequest, CustomExpressResponse } from "./types";
 
 const DEFAULT_API_VERSION = "1.0";
 const API_BASE = `http://${config.apiHost}:${config.apiPort}/api`;
@@ -17,7 +17,7 @@ const transformRequest: AxiosRequestTransformer = (data) => {
   if (typeof data === "object") {
     return {
       apiVersion: DEFAULT_API_VERSION,
-      data: {...data},
+      data: { ...data },
     };
   } else {
     return data;
@@ -28,7 +28,7 @@ axios.defaults.transformRequest = [transformRequest];
 const excelService = express();
 excelService.use(createPinoExpressLogger(logger));
 excelService.use(express.json());
-excelService.use(cors({origin: config.accessControlAllowOrigin}));
+excelService.use(cors({ origin: config.accessControlAllowOrigin }));
 excelService.use((req: CustomExpressRequest, res: CustomExpressResponse, next) => {
   res.apiBase = API_BASE;
   next();
@@ -42,7 +42,7 @@ excelService.use((req: CustomExpressRequest, res: CustomExpressResponse, next) =
 excelService.get("/liveness", (req, res) => {
   res
     .status(200)
-    .header({"Content-Type": "application/json"})
+    .header({ "Content-Type": "application/json" })
     .send(
       JSON.stringify({
         uptime: process.uptime(),
@@ -59,7 +59,7 @@ excelService.get("/readiness", async (req: CustomExpressRequest, res: CustomExpr
       return res.status(504).send("Not ready. Waiting for API");
     }
   } catch (error) {
-    logger.error({error}, "Error during readiness check on API");
+    logger.error({ error }, "Error during readiness check on API");
     return res.status(504).send("Error during readiness check");
   }
 });
@@ -85,11 +85,11 @@ excelService.get("/download", async (req: CustomExpressRequest, res: CustomExpre
     await getApiVersion(axios, token, res.apiBase);
   } catch (err) {
     if (err.response?.status == 401) {
-      logger.error({err}, "Invalid Token:");
-      res.status(err.response.status).send({message: err.response.data});
+      logger.error({ err }, "Invalid Token:");
+      res.status(err.response.status).send({ message: err.response.data });
     } else {
-      logger.error({err}, "Error validating token");
-      res.status(err.response.status).send({message: `Error validating token: ${err.response} `});
+      logger.error({ err }, "Error validating token");
+      res.status(err.response.status).send({ message: `Error validating token: ${err.response} ` });
     }
   }
 
@@ -106,9 +106,9 @@ excelService.get("/download", async (req: CustomExpressRequest, res: CustomExpre
 
     await writeXLSX(axios, token, res);
   } catch (error) {
-    req.log.error({err: error}, "Error while creating excel export");
+    req.log.error({ err: error }, "Error while creating excel export");
     if (error.response) {
-      res.status(error.response.status).send({message: error.response.data});
+      res.status(error.response.status).send({ message: error.response.data });
     }
   }
 });
@@ -117,12 +117,12 @@ excelService.listen(config.serverPort, () => logger.info(`App listening on ${con
 
 // Enable useful traces of unhandled-promise warnings:
 process.on("unhandledRejection", (err) => {
-  logger.fatal({err}, "UNHANDLED PROMISE REJECTION");
+  logger.fatal({ err }, "UNHANDLED PROMISE REJECTION");
   process.exit(1);
 });
 
 function setExcelLanguage(url: string): void {
-  logger.debug({url}, "Set excel language based on url");
+  logger.debug({ url }, "Set excel language based on url");
   const queryData = URL.parse(url, true).query;
 
   if (queryData.lang) {
