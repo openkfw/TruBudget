e935418eddd48e9e85384361faece09076ce380c
egdmrsy <megedemirsoy@gmail.com>
2023-04-24 12:48:42 +0200

    ui:updating e2e tests

--- e2e-test/cypress/integration/workflowitem_reject_spec.js
+++ e2e-test/cypress/integration/workflowitem_reject_spec.js
@@ -1,3 +1,5 @@
+import {toAmountString} from "../support/helper";
+
 describe("Workflowitem reject", function () {
   let projectId;
   let subprojectId;
@@ -23,13 +25,14 @@ describe("Workflowitem reject", function () {
   beforeEach(function () {
     cy.login();
     cy.visit(`/projects/${projectId}/${subprojectId}`);
+  });
+
+  it("When rejecting a workflow item, a reason must be provided", function () {
     cy.createWorkflowitem(projectId, subprojectId, "workflowitem assign test").then(({id}) => {
       workflowitemId = id;
       cy.grantWorkflowitemPermission(projectId, subprojectId, workflowitemId, "workflowitem.list", testUser.id);
     });
-  });
 
-  it("When rejecting a workflow item, a reason must be provided", function () {
     // testUser may not reject the workflowitem
     cy.login(testUser.id, testUser.password);
     cy.visit(`/projects/${projectId}/${subprojectId}`);
@@ -47,6 +50,11 @@ describe("Workflowitem reject", function () {
   it("The workflowitem can be rejected by the assignee only", function () {
     cy.intercept(apiRoute + `/workflowitem.close`).as("workflowitemClose");
 
+    cy.createWorkflowitem(projectId, subprojectId, "workflowitem assign test").then(({id}) => {
+      workflowitemId = id;
+      cy.grantWorkflowitemPermission(projectId, subprojectId, workflowitemId, "workflowitem.list", testUser.id);
+    });
+
     // the assignee may reject the workflowitem
     cy.login();
     cy.visit(`/projects/${projectId}/${subprojectId}`);
@@ -63,5 +71,71 @@ describe("Workflowitem reject", function () {
       .click();
     cy.get("[data-test=infromation-dialog-content]").should("contain", rejectReason);
     cy.get("[data-test=infromation-dialog-close]").click();
+  });
+
+  it("Rejected workflowitem's allocated amount should not be added to assigned budget", function () {
+    cy.intercept(apiRoute + `/workflowitem.close`).as("workflowitemClose");
+
+    cy.createWorkflowitem(projectId, subprojectId, "workflowitem assign test", {
+      amountType: "allocated",
+      amount: "1000",
+      currency: "EUR"
+    }).then(({id}) => {
+      workflowitemId = id;
+      cy.grantWorkflowitemPermission(projectId, subprojectId, workflowitemId, "workflowitem.list", testUser.id);
+    });
+
+    // the assignee may reject the workflowitem
+    cy.login();
+    cy.visit(`/projects/${projectId}/${subprojectId}`);
+    cy.get("[data-test=reject-workflowitem]")
+      .last()
+      .click({force: true});
+    cy.get("[data-test=reject-workflowitem-reject-reason]").type(rejectReason);
+    cy.get("[data-test=confirmation-dialog-confirm]").should("not.be.disabled");
+    cy.get("[data-test=confirmation-dialog-confirm]").click({force: true});
+    cy.wait("@workflowitemClose");
+
+    cy.get("[data-test=details-analytics-button]")
+      .should("be.visible")
+      .click();
+
+    cy.get("[data-test=number-chart-assigned-budget]").should(
+      "have.text",
+      toAmountString(0, "EUR")
+    );
+  });
+
+  it("Rejected workflowitem's disbursed amount should not be added to paid budget", function () {
+    cy.intercept(apiRoute + `/workflowitem.close`).as("workflowitemClose");
+
+    cy.createWorkflowitem(projectId, subprojectId, "workflowitem assign test", {
+      amountType: "disbursed",
+      amount: "1000",
+      currency: "EUR"
+    }).then(({id}) => {
+      workflowitemId = id;
+      cy.grantWorkflowitemPermission(projectId, subprojectId, workflowitemId, "workflowitem.list", testUser.id);
+    });
+
+    // the assignee may reject the workflowitem
+    cy.login();
+    cy.visit(`/projects/${projectId}/${subprojectId}`);
+    cy.get("[data-test=reject-workflowitem]")
+      .last()
+      .click({force: true});
+    cy.get("[data-test=reject-workflowitem-reject-reason]").type(rejectReason);
+    cy.get("[data-test=confirmation-dialog-confirm]").should("not.be.disabled");
+    cy.get("[data-test=confirmation-dialog-confirm]").click({force: true});
+    cy.wait("@workflowitemClose");
+
+    cy.get("[data-test=details-analytics-button]")
+      .should("be.visible")
+      .click();
+
+    cy.get("[data-test=number-chart-disbursed-budget]").should(
+      "have.text",
+      toAmountString(0, "EUR")
+    );
   });
 });
