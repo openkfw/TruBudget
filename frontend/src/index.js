9298063c45e3ba488402310c15c8e0f881b370fc
Samuel Pull <samuel.pull@accenture.com>
2023-05-15 14:46:06 +0200

    ui: disable React.StrictMode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,4 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
