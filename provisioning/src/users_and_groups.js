c262f466d86abea7a59cfa110e1b7be9cf4110cd
egdmrsy <megedemirsoy@gmail.com>
2023-10-30 12:19:58 +0100

    prov:added dynamic user permissions feature

--- provisioning/src/users_and_groups.js
+++ provisioning/src/users_and_groups.js
@@ -3,24 +3,48 @@ const {
   createUser,
   createGroup,
   addUserToGroup,
-  removeUserFromGroup,
   grantAllPermissionsToUser,
+  grantGlobalPermissionToUser,
 } = require("./api");
 const log = require("./logger");
+
+const allowedIntents = [
+  "global.listPermissions",
+  "global.grantPermission",
+  "global.grantAllPermissions",
+  "global.revokePermission",
+  "global.createProject",
+  "global.createUser",
+  "global.enableUser",
+  "global.disableUser",
+  "global.listAssignments",
+  "global.createGroup",
+  "network.registerNode",
+  "network.list",
+  "network.listActive",
+  "network.voteForPermission",
+  "network.approveNewOrganization",
+  "network.approveNewNodeForExistingOrganization",
+  "network.declineNode",
+  "provisioning.start",
+  "provisioning.end",
+  "provisioning.get"
+];
 
 const provisionUsers = async (axios, folder, organization) => {
   try {
     const users = readJsonFile(folder + "users.json");
-    for (const user of users) {
-      log.info(`~> Adding user ${user.displayName}`);
-      await createUser(axios, user, organization);
+
+    await Promise.allSettled(users.map((user) => createUser(axios, {id: user.id, displayName: user.displayName, password: user.password}, organization)));
+    const usersWithPermissions = users.filter((user) => Array.isArray(user.permissions) && user.permissions.length > 0);
+    for (const user of usersWithPermissions) {
+      if (user.permissions.includes("all")) {
+        await grantAllPermissionsToUser(axios, user.id);
+      } 
+      else {
+        await Promise.allSettled(user.permissions.filter(intent => allowedIntents.includes(intent)).map((intent) => grantGlobalPermissionToUser(axios, intent, user.id)));
+      }
     }
-
-    await grantAllPermissionsToUser(axios, "mstein");
-    log.info("~> all Permissions granted to mstein");
-
-    await grantAllPermissionsToUser(axios, "jdoe");
-    log.info("~> all Permissions granted to jdoe");
   } catch (err) {
     if (err.code && err.code === "MAX_RETRIES") {
       log.info("Failed to provision users, max retries exceeded");
@@ -34,23 +58,12 @@ const provisionUsers = async (axios, folder, organization) => {
 
 const provisionGroups = async (axios, folder) => {
   try {
-    const users = readJsonFile(folder + "users.json");
     const groups = readJsonFile(folder + "groups.json");
-    for (const group of groups) {
-      log.info(`~> Adding group ${group.displayName}`);
-      await createGroup(axios, group);
+    await Promise.allSettled(groups.map((group) => createGroup(axios, group)));
+    const groupsWithUsers = groups.filter((group) => Array.isArray(group.users) && group.users.length > 0);
+    for (const group of groupsWithUsers) {
+      await Promise.allSettled(group.users.map((user) => addUserToGroup(axios, group.id, user)));
     }
-
-    log.info("~> Adding user jxavier to group 'Reviewers'");
-    await addUserToGroup(axios, "reviewers", "jxavier");
-    log.info("~> Adding user pkleffmann to group 'Reviewers'");
-    await addUserToGroup(axios, "reviewers", "pkleffmann");
-    log.info("~> Adding user dviolin to group 'Reviewers'");
-    await addUserToGroup(axios, "reviewers", "mstein");
-    log.info("~> Adding user mstein to group 'Reviewers'");
-    await addUserToGroup(axios, "reviewers", "dviolin");
-    log.info("~> Removing user dviolin from group 'Reviewers'");
-    await removeUserFromGroup(axios, "reviewers", "dviolin");
   } catch (err) {
     if (err.code && err.code === "MAX_RETRIES") {
       log.info("Failed to provision groups, max retries exceeded");
