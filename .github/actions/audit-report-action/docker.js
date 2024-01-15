fca4825c916ae9ad9965534e75d89943532e1d94
Samuel Pull <samuel.pull@accenture.com>
2024-01-15 09:26:15 +0100

    cicd: audit main images

--- .github/actions/audit-report-action/docker.js
+++ .github/actions/audit-report-action/docker.js
@@ -3,11 +3,11 @@ import { Config } from "./config";
 const child_process = require('child_process');
 
 export async function pullImage(imageName) {
-  child_process.spawnSync("docker", ["pull", `trubudget/${imageName}`], {
+  child_process.spawnSync("docker", ["pull", `trubudget/${imageName}:main`], {
     encoding: 'utf-8',
     maxBuffer: Config.spawnProcessBufferSize
   });
-  child_process.spawnSync("docker", ["save", `trubudget/${imageName}`, "-o", `${imageName}.tar`], {
+  child_process.spawnSync("docker", ["save", `trubudget/${imageName}:main`, "-o", `${imageName}.tar`], {
     encoding: 'utf-8',
     maxBuffer: Config.spawnProcessBufferSize
   });
