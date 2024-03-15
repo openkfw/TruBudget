a735b75483863b3373c12d43151c7954d398014d
SamuelPull <samuel.pull@accenture.com>
2024-03-15 11:45:06 +0100

    api, ui: validate document client-side (#1719)

--- frontend/src/pages/Documents/reducer.js
+++ frontend/src/pages/Documents/reducer.js
@@ -1,6 +1,6 @@
 import { fromJS } from "immutable";
 
-import { CLEAR_DOCUMENTS, VALIDATE_DOCUMENT, VALIDATE_DOCUMENT_SUCCESS } from "./actions";
+import { CLEAR_DOCUMENTS, VALIDATE_DOCUMENT, VALIDATE_DOCUMENT_FE, VALIDATE_DOCUMENT_SUCCESS } from "./actions";
 
 const defaultState = fromJS({
   validatedDocuments: {},
@@ -11,6 +11,8 @@ export default function documentsReducer(state = defaultState, action) {
   switch (action.type) {
     case VALIDATE_DOCUMENT:
       return state.set("documentToValidate", { id: action.id, hash: action.hash });
+    case VALIDATE_DOCUMENT_FE:
+      return state.set("documentToValidate", { id: action.documentId, hash: action.hash });
     case VALIDATE_DOCUMENT_SUCCESS:
       return state.setIn(["validatedDocuments", state.getIn(["documentToValidate"]).id], action.isIdentical);
     case CLEAR_DOCUMENTS:
