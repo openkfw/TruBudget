b4b74b3ccbc8a8687bc73f7f0af2a7a73daa8a9f
Samuel Pull <samuel.pull@accenture.com>
2024-03-22 16:31:30 +0100

    unit tests

--- api/src/lib/chunkArray.ts
+++ api/src/lib/chunkArray.ts
@@ -1,6 +1,26 @@
+import logger from "./logger";
+
 export function chunkArray<T>(items: T[], size: number | string): T[][] {
-  // TODO see what happens if size parameter is 0 ?
-  const chunkSize = typeof size === "string" ? parseInt(size) : size;
+  let chunkSize: number;
+
+  if (typeof size === "string") {
+    chunkSize = parseInt(size);
+    if (isNaN(chunkSize)) {
+      logger.info(`Invalid chunk size "${size}", defaulting to 10`);
+      chunkSize = 10;
+    }
+  } else if (typeof size === "number") {
+    if (size <= 0 || isNaN(size)) {
+      logger.info(`Invalid chunk size ${size}, defaulting to 10`);
+      chunkSize = 10;
+    } else {
+      chunkSize = size;
+    }
+  } else {
+    logger.info(`Invalid chunk size ${size}, defaulting to 10`);
+    chunkSize = 10;
+  }
+
   const chunks: Array<T[]> = [];
   for (let i = 0; i < items.length; i += chunkSize) {
     chunks.push(items.slice(i, i + chunkSize));
