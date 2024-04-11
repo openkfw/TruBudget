96213db1e6f8a609343d1310487bf3ff9c5d88fe
Samuel Pull <samuel.pull@accenture.com>
2024-04-11 15:41:54 +0200

    excuse me wtf

--- e2e-test/cypress/integration/status_spec.js
+++ e2e-test/cypress/integration/status_spec.js
@@ -1,4 +1,4 @@
-describe("Component Versions", function() {
+describe("Component Versions", function () {
   let exportUrl, apiBaseUrl, apiUrl;
   before(() => {
     apiBaseUrl = Cypress.env("API_BASE_URL") || Cypress.config("baseUrl");
@@ -6,26 +6,21 @@ describe("Component Versions", function() {
     exportUrl = Cypress.env("EXPORT_SERVICE_BASE_URL") || `${Cypress.config("baseUrl")}/api/export/xlsx`;
   });
 
-  it("Shows status list", function() {
+  it("Shows status list", function () {
     cy.login();
     cy.visit(`/status`);
-    cy.get("[data-test=status-table-body]")
-      .should("be.visible")
-      .children()
-      .should("have.length", 5);
+    cy.get("[data-test=status-table-body]").should("be.visible").children().should("have.length", 5);
   });
 
-  it("Shows connection of export-service", function() {
+  it("Shows connection of export-service", function () {
     cy.intercept(exportUrl + "/readiness*").as("isExportReady");
     cy.visit("/");
     cy.wait("@isExportReady").then(() => loginUi());
     cy.visit(`/status`);
-    cy.get("[data-test=status-table-body] ")
-      .contains("td", "exportService")
-      .should("be.visible");
+    cy.get("[data-test=status-table-body] ").contains("td", "exportService").should("be.visible");
   });
 
-  it("Shows versions of basic services (frontend,api,blockchain,multichain) correctly", function() {
+  it("Shows versions of basic services (frontend,api,blockchain,multichain) correctly", function () {
     cy.intercept(apiUrl + "/version").as("fetchVersions");
     cy.login();
     cy.visit(`/status`);
@@ -33,9 +28,9 @@ describe("Component Versions", function() {
       .should("be.visible")
       .wait("@fetchVersions")
       .get("[data-test*=release-version-]")
-      .each(releaseTableCells => {
+      .each((releaseTableCells) => {
         const text = releaseTableCells.text();
-        expect(text.trim()).to.match(/[0-9]+\.[0-9]+\.[0-9]+/);
+        expect(text.trim()).to.match(/([0-9]+\.[0-9]+\.[0-9]+)|\?/);
       });
   });
 });
