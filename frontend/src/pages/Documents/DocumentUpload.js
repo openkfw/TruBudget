e6927f6c51145eda25c9716bc597845f463e5c8c
Peter Baus <peter.baus@accenture.com>
2024-08-08 09:15:27 +0200

    frontend:Split upload to 2 boxes

--- frontend/src/pages/Documents/DocumentUpload.js
+++ frontend/src/pages/Documents/DocumentUpload.js
@@ -4,13 +4,14 @@ import * as Yup from "yup";
 import AddLinkIcon from "@mui/icons-material/AddLink";
 import LinkIcon from "@mui/icons-material/Link";
 import UploadIcon from "@mui/icons-material/Publish";
-import { TextField, Tooltip } from "@mui/material";
+import { Grid, Paper, TableHead, TextField, Tooltip } from "@mui/material";
 import Button from "@mui/material/Button";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
+import { Stack } from "@mui/system";
 
 import strings from "../../localizeStrings";
 
@@ -94,33 +95,41 @@ const DocumentUpload = ({
     storeWorkflowDocumentExternalLink(externalDocumentUrl, externalDocumentName, externalDocumentHash);
     setExternalDocumentUrl(defaultDocumentUrl);
     setExternalDocumentName(defaultDocumentName);
+    setExternalDocumentHash();
   };
 
   const body = (
     <TableBody>
       {workflowDocuments.length > 0 ? (
-        workflowDocuments.map((document, index) => (
-          <TableRow key={`${index}-${document.fileName}`}>
-            <TableCell className="document-link-cell" data-test="workflowitemDocumentFileName">
-              <Typography variant="body1" component="div">
-                {document.fileName}
-                {document.link && (
-                  <Tooltip title={document.link}>
-                    <Button
-                      onClick={(event) => {
-                        event.preventDefault();
-                        window.open(document.link, "_blank");
-                      }}
-                      component="div"
-                    >
-                      <LinkIcon />
-                    </Button>
-                  </Tooltip>
-                )}
-              </Typography>
-            </TableCell>
-          </TableRow>
-        ))
+        <>
+          <TableHead>
+            <TableRow>
+              <TableCell>Documents/Links to save</TableCell>
+            </TableRow>
+          </TableHead>
+          {workflowDocuments.map((document, index) => (
+            <TableRow key={`${index}-${document.fileName}`}>
+              <TableCell className="document-link-cell" data-test="workflowitemDocumentFileName">
+                <Typography variant="body1" component="div">
+                  {document.fileName}
+                  {document.link && (
+                    <Tooltip title={document.link}>
+                      <Button
+                        onClick={(event) => {
+                          event.preventDefault();
+                          window.open(document.link, "_blank");
+                        }}
+                        component="div"
+                      >
+                        <LinkIcon />
+                      </Button>
+                    </Tooltip>
+                  )}
+                </Typography>
+              </TableCell>
+            </TableRow>
+          ))}
+        </>
       ) : (
         <TableRow>
           <TableCell className="document-empty-state-bg">
@@ -136,109 +145,124 @@ const DocumentUpload = ({
       <div className="document-upload-container">
         <Table className="document-upload-table">{body}</Table>
       </div>
-      {storageServiceAvailable && (
-        <>
-          <div className="document-upload-flex-container">
-            <Button className="document-upload-button" component="div">
-              <UploadIcon />
-              {strings.workflow.workflow_upload_document}
-              <input
-                id="docupload"
-                type="file"
-                className="document-upload-input"
-                onChange={(event) => {
-                  if (event.target.files) {
-                    const file = event.target.files[0];
-                    if (file.size > MAX_DOCUMENT_SIZE_BINARY) {
-                      storeSnackbarMessage(
-                        `File size exceeds the limit of ${Math.round(MAX_DOCUMENT_SIZE_BINARY / (1024 * 1024))} MB`
-                      );
-                      showErrorSnackbar();
-                      return;
-                    }
-                    const reader = new FileReader();
-                    reader.onloadend = (e) => {
-                      if (e.target.result !== undefined) {
-                        const dataUrl = e.target.result.split(";base64,")[1];
-                        // data in redux store needs to be serializable, so we store base64 string
-                        storeWorkflowDocument(dataUrl, file.name);
+      <Grid container spacing={2}>
+        {storageServiceAvailable && (
+          <Grid item xs={6}>
+            <Paper>
+              <div className="document-upload-flex-container">
+                <Button className="document-upload-button" component="div">
+                  <UploadIcon />
+                  {strings.workflow.workflow_upload_document}
+                  <input
+                    id="docupload"
+                    type="file"
+                    className="document-upload-input"
+                    onChange={(event) => {
+                      if (event.target.files) {
+                        const file = event.target.files[0];
+                        if (file.size > MAX_DOCUMENT_SIZE_BINARY) {
+                          storeSnackbarMessage(
+                            `File size exceeds the limit of ${Math.round(MAX_DOCUMENT_SIZE_BINARY / (1024 * 1024))} MB`
+                          );
+                          showErrorSnackbar();
+                          return;
+                        }
+                        const reader = new FileReader();
+                        reader.onloadend = (e) => {
+                          if (e.target.result !== undefined) {
+                            const dataUrl = e.target.result.split(";base64,")[1];
+                            // data in redux store needs to be serializable, so we store base64 string
+                            storeWorkflowDocument(dataUrl, file.name);
+                          }
+                        };
+                        if (file) {
+                          reader.readAsDataURL(file);
+                        }
                       }
-                    };
-                    if (file) {
-                      reader.readAsDataURL(file);
-                    }
-                  }
-                }}
-              />
-            </Button>
-          </div>
-          <div className="document-upload-flex-container">
-            <div className="or-label">or</div>
-          </div>
-        </>
-      )}
-      <div className="document-upload-flex-container">
-        <TextField
-          id="external-document-url"
-          label="External link URL"
-          value={externalDocumentUrl}
-          onChange={handleExternalDocumentUrlChange}
-          error={externalDocumentUrlError}
-          helperText={externalDocumentUrlHelperText}
-          className="document-external-link-input"
-        />
-        <TextField
-          id="external-document-url"
-          label="External document name"
-          value={externalDocumentName}
-          onChange={handleExternalDocumentNameChange}
-          error={externalDocumentNameError}
-          helperText={externalDocumentNameHelperText}
-        />
-        {externalDocumentHash ? (
-          "Hash created"
-        ) : (
-          <Button className="document-upload-button" component="div">
-            <UploadIcon />
-            Upload same document to create hash
-            <input
-              id="docupload"
-              type="file"
-              className="document-upload-input"
-              onChange={(event) => {
-                if (event.target.files[0]) {
-                  const file = event.target.files[0];
-                  const reader = new FileReader();
-                  reader.onloadend = async (e) => {
-                    if (e.target.result !== undefined) {
-                      const dataBase64 = e.target.result.split(";base64,")[1];
-                      const newHash = await hashValue(dataBase64);
-                      setExternalDocumentHash(newHash);
-                    }
-                  };
-                  reader.readAsDataURL(file);
-                }
-              }}
-            />
-          </Button>
+                    }}
+                  />
+                </Button>
+              </div>
+            </Paper>
+          </Grid>
         )}
-      </div>
-      <div className="document-upload-flex-container">
-        <Button
-          onClick={addExternalLink}
-          className="document-upload-button"
-          component="div"
-          disabled={
-            externalDocumentUrlError ||
-            externalDocumentNameError ||
-            !externalDocumentUrlUpdated ||
-            !externalDocumentNameUpdated
-          }
-        >
-          <AddLinkIcon />
-          Add external link
-        </Button>
-      </div>
+        <Grid item xs={6}>
+          <Paper>
+            <div style={{ margin: "0 10px" }}>
+              <Stack
+                component="form"
+                sx={{
+                  width: "25ch"
+                }}
+                spacing={2}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  id="external-document-url"
+                  label="External link URL"
+                  value={externalDocumentUrl}
+                  onChange={handleExternalDocumentUrlChange}
+                  error={externalDocumentUrlError}
+                  helperText={externalDocumentUrlHelperText}
+                  className="document-external-link-input"
+                />
+                <TextField
+                  id="external-document-url"
+                  label="External document name"
+                  value={externalDocumentName}
+                  onChange={handleExternalDocumentNameChange}
+                  error={externalDocumentNameError}
+                  helperText={externalDocumentNameHelperText}
+                />
+              </Stack>
+              {externalDocumentHash ? (
+                "Hash created"
+              ) : (
+                <Button className="document-upload-button" component="div">
+                  <UploadIcon />
+                  Upload same document to create hash
+                  <input
+                    id="docupload"
+                    type="file"
+                    className="document-upload-input"
+                    onChange={(event) => {
+                      if (event.target.files[0]) {
+                        const file = event.target.files[0];
+                        const reader = new FileReader();
+                        reader.onloadend = async (e) => {
+                          if (e.target.result !== undefined) {
+                            const dataBase64 = e.target.result.split(";base64,")[1];
+                            const newHash = await hashValue(dataBase64);
+                            setExternalDocumentHash(newHash);
+                          }
+                        };
+                        reader.readAsDataURL(file);
+                      }
+                    }}
+                  />
+                </Button>
+              )}
+            </div>
+            <div className="document-upload-flex-container">
+              <Button
+                onClick={addExternalLink}
+                className="document-upload-button"
+                component="div"
+                disabled={
+                  externalDocumentUrlError ||
+                  externalDocumentNameError ||
+                  !externalDocumentUrlUpdated ||
+                  !externalDocumentNameUpdated
+                }
+              >
+                <AddLinkIcon />
+                Add external link
+              </Button>
+            </div>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
