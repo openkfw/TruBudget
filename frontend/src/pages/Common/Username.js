1d6c342c8d78dd7af8ad8ef8f3eaed14f2220276
Peter Molnar <p.molnar@accenture.com>
2024-10-31 09:12:57 +0100

    new design for login page

--- frontend/src/pages/Common/Username.js
+++ frontend/src/pages/Common/Username.js
@@ -1,21 +1,19 @@
 import React from "react";
 
-import UsernameIcon from "@mui/icons-material/Person";
-
 import strings from "../../localizeStrings";
 
 import TextInputWithIcon from "./TextInputWithIcon";
 
-const Username = ({ username, storeUsername, failed, id, failedText, label, ...props }) => {
+const Username = ({ username, storeUsername, failed, id, failedText, label, icon, ...props }) => {
   return (
     <TextInputWithIcon
-      style={{ width: "50%" }}
+      style={{ width: "70%" }}
       label={label || strings.common.username}
       value={username}
       margin="normal"
       error={failed}
       id={id}
-      icon={<UsernameIcon />}
+      icon={icon}
       onChange={(event) => storeUsername(event.target.value)}
       helperText={failed ? failedText : null}
       {...props}
